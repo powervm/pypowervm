a6736c936f0454e6f556749220afface79d480f0
Drew Thorstensen <thorst@us.ibm.com>
2015-01-19 12:27:05 -0600

    Adding client network adapters
    Provides a wrapper to Client Network Adapters.
    Includes a job that allows the creation of the client network adapter.
    
    Change-Id: Ia570d79dd8fbc3a16aafeabecd997d3834142645

--- pypowervm/wrappers/network.py
+++ pypowervm/wrappers/network.py
@@ -16,14 +16,150 @@
 
 import logging
 
+from pypowervm import adapter as adpt
+from pypowervm import util as u
 import pypowervm.wrappers.constants as c
 import pypowervm.wrappers.entry_wrapper as ewrap
 
 LOG = logging.getLogger(__name__)
 
+LOCATION_CODE = 'LocationCode'
+
+VADPT_SLOT_NUM = 'VirtualSlotNumber'
+VADPT_MAC_ADDR = 'MACAddress'
+VADPT_TAGGED_VLANS = 'TaggedVLANIDs'
+VADPT_TAGGED_VLAN_SUPPORT = 'TaggedVLANSupported'
+VADPT_VSWITCH = 'AssociatedVirtualSwitch'
+
+VSW_NAME = 'SwitchName'
+VSW_ID = 'SwitchID'
+VSW_MODE = 'SwitchMode'
+
+
+def crt_cna(pvid, vswitch_href, slot_num=None, mac_addr=None,
+            addl_tagged_vlans=None):
+    """Creates the Element structure for the creation of a Client Network Adpt.
+
+    This is used when creating a new CNA for a client partition.  The POST
+    of this should be done to the LogicalPartition/<UUID>/ClientNetworkAdapter.
+
+    :param pvid: The Primary VLAN ID to use.
+    :param vswitch_href: The URI that points to the Virtual Switch that will
+                         support this adapter.
+    :param slot_num: The Slot on the Client LPAR that should be used.  This
+                     defaults to 'None', which means that the next available
+                     slot will be used.
+    :param mac_addr: The optional user specified mac address to use.  If left
+                     as None, the system will generate one.
+    :param addl_tagged_vlans: A set of additional tagged VLANs that can be
+                              passed through this adapter (with client VLAN
+                              adapters).
+
+                              Input should be a space delimited string.
+                              Example: '51 52 53'
+                              Note: The limit is ~18 additional VLANs
+    :returns: An Element that can be used for a Client Network Adapter create.
+    """
+    attrs = []
+    if slot_num is None:
+        attrs.extend(adpt.Element(VADPT_SLOT_NUM, text=str(slot_num)))
+    else:
+        attrs.extend(adpt.Element('UseNextAvailableSlotID', text='true'))
+
+    if mac_addr is not None:
+        mac_addr = u.sanitize_mac_for_api(mac_addr)
+        attrs.extend(adpt.Element(VADPT_MAC_ADDR), text=mac_addr)
+
+    if addl_tagged_vlans is not None:
+        attrs.extend(adpt.Element(VADPT_TAGGED_VLANS), text=addl_tagged_vlans)
+        attrs.extend(adpt.Element(VADPT_TAGGED_VLAN_SUPPORT, text='true'))
+    else:
+        attrs.extend(adpt.Element(VADPT_TAGGED_VLAN_SUPPORT, text='false'))
+
+    attrs.extend(adpt.Element(c.PORT_VLAN_ID, text=str(pvid)))
+
+    # Put in the vSwitch
+    vswitch_child = adpt.Element('link', attrib={'href': vswitch_href,
+                                                 'rel': 'related'})
+    assoc_vswitch = adpt.Element(VADPT_VSWITCH, children=[vswitch_child])
+    attrs.extend(assoc_vswitch)
+
+    return adpt.Element('ClientNetworkAdapter', attrib=c.DEFAULT_SCHEMA_ATTR,
+                        children=attrs)
+
+
+class VirtualSwitch(ewrap.EntryWrapper):
+    """Wraps the Virtual Switch entries.
+
+    The virtual switch in PowerVM is an independent plan of traffic.  If
+    Ethernet packets are traveling on different virtual switches, the only
+    time they can communicate is on the physical network plane (or if two
+    logical adapters are bridged together).  They are important for data
+    plan segregation.
+    """
+
+    def get_name(self):
+        """The name associated with the Virtual Switch."""
+        return self.get_parm_value(VSW_NAME)
+
+    def get_switch_id(self):
+        """The internal ID (not UUID) for the Virtual Switch."""
+        return self.get_parm_value_int(VSW_ID)
+
+    def get_mode(self):
+        """The mode that the switch is in (ex. VEB)."""
+        return self.get_parm_value(VSW_MODE)
+
+
+class ClientNetworkAdapter(ewrap.EntryWrapper):
+    """Wrapper object for the ClientNetworkAdapter entry.
+
+    A ClientNetworkAdapter (cna) is a Virtual Ethernet Adapter that is not set
+    for bridging.  This means that it is the 'target' adapter for end client
+    communication.  While these may exist on a Virtual I/O Server, they are
+    typically used for client LPARs instead.
+    """
+
+    def get_loc_code(self):
+        """The device's location code."""
+        return self.get_parm_value(LOCATION_CODE)
+
+    def get_slot_num(self):
+        """The slot number for the adapter."""
+        return self.get_parm_value_int(VADPT_SLOT_NUM)
+
+    def get_mac_addr(self):
+        """The mac address on this adapter."""
+        return self.get_parm_value(VADPT_MAC_ADDR)
+
+    def get_pvid(self):
+        """Returns the Primary VLAN ID of the adapter."""
+        return self.get_parm_value_int(c.PORT_VLAN_ID)
+
+    def get_tagged_vlans(self):
+        """Returns a list of additional VLANs on this adapter.
+
+        Only valid if tagged vlan support is on.
+        """
+        addl_vlans = self.get_parm_value(VADPT_TAGGED_VLANS, '')
+        return addl_vlans.split(' ')
+
+    def is_tagged_vlan_supported(self):
+        """Returns if addl tagged VLANs are supported."""
+        return self.get_parm_value_bool(VADPT_TAGGED_VLAN_SUPPORT)
+
+    def get_vswitch_uri(self):
+        """Returns the URI for the associated vSwitch."""
+        vswitches = self._entry.element.findall(VADPT_VSWITCH + c.DELIM +
+                                                'link')
+        if len(vswitches) != 1:
+            return ''
+        vs_elem = vswitches[1]
+        return vs_elem.attr['href']
+
 
 class NetworkBridge(ewrap.EntryWrapper):
-    """Wrapper object for the NetworkBridge element.
+    """Wrapper object for the NetworkBridge entry.
 
     A NetworkBridge represents an aggregate entity comprising Shared
     Ethernet Adapters.  If Failover or Load-Balancing is in use, the
