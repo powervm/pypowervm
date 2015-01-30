f15b32a674b0f23717fd3e29ce6f8946cc8f35e9
Kyle L. Henderson <kyleh@us.ibm.com>
2015-01-30 11:09:18 -0600

    Add retry decorator
    There are a few cases when a retry is needed when interacting with
    PowerVM.
    
    * When updating a resource and the etag has changed between the
    read and the update operation.
    
    * When the VIOS reports an rmc status as ebusy. In this case, the
    data returned may be incomplete and is not usable for an update
    operation.
    
    The retry decorator can be used to decorate a method to do
    retries.
    
    Change-Id: I89b0a5e1352ca086757b536837469d6e2e3e0757

---
+++ pypowervm/tests/utils/test_retry.py
@@ -0,0 +1,151 @@
+# Copyright 2015 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import unittest
+
+from pypowervm import adapter as adpt
+from pypowervm import exceptions as pvm_exc
+from pypowervm.utils import retry as pvm_retry
+
+called_count = 0
+
+
+class TestRetry(unittest.TestCase):
+    """Unit tests for pypowervm.util."""
+
+    def test_retry(self):
+        global called_count
+
+        # Test normal call with return values
+        @pvm_retry.retry(tries=4)
+        def some_method(x, y):
+            return x, y
+
+        val = some_method(1, 2)
+        self.assertEqual(val, (1, 2))
+
+        # Test with an unexpected exception
+        class OurException(Exception):
+            pass
+
+        @pvm_retry.retry(tries=4)
+        def except_method(x, y):
+            global called_count
+            called_count += 1
+            raise OurException()
+
+        self.assertRaises(OurException, except_method, 1, 2)
+        self.assertEqual(called_count, 1)
+
+        # Test retry with an http code
+        @pvm_retry.retry(tries=4, http_codes=(412,))
+        def http_except_method(x, y):
+            global called_count
+            called_count += 1
+            resp = adpt.Response('reqmethod', 'reqpath', 412,
+                                 'reason', 'headers')
+            http_exc = pvm_exc.HttpError('msg', resp)
+            raise http_exc
+
+        called_count = 0
+        self.assertRaises(pvm_exc.HttpError, http_except_method, 1, 2)
+        self.assertEqual(called_count, 4)
+
+        # Test retry with an test func and custom exception
+        def cust_test(e, try_, tries, *args, **kwds):
+            return try_ != 2
+
+        @pvm_retry.retry(tries=10, test_func=cust_test,
+                         limit_except=OurException())
+        def func_except_method(x, y):
+            global called_count
+            called_count += 1
+            resp = adpt.Response('reqmethod', 'reqpath', 412,
+                                 'reason', 'headers')
+            http_exc = pvm_exc.HttpError('msg', resp)
+            raise http_exc
+
+        called_count = 0
+        # Should get back OurException after just 2 calls
+        self.assertRaises(OurException, func_except_method, 1, 2)
+        self.assertEqual(called_count, 2)
+
+        # Test custom exceptions to retry
+        @pvm_retry.retry(tries=3, retry_except=OurException)
+        def func_except_method(x, y):
+            global called_count
+            called_count += 1
+            raise OurException()
+
+        called_count = 0
+        # Should get back OurException after just 3 calls
+        self.assertRaises(OurException, func_except_method, 1, 2)
+        self.assertEqual(called_count, 3)
+
+        # Test the response checking function
+        def resp_chkr(resp, try_, tries, *args, **kwds):
+            if try_ == 2:
+                raise OurException()
+            # Tell it to retry
+            return True
+
+        @pvm_retry.retry(tries=10, resp_checker=resp_chkr)
+        def func_resp_chkr(x, y):
+            global called_count
+            called_count += 1
+            return x, y
+
+        called_count = 0
+        # Should get back OurException after just 2 calls
+        self.assertRaises(OurException, func_resp_chkr, 1, 2)
+        self.assertEqual(called_count, 2)
+
+    def test_retry_example(self):
+        global called_count
+        called_count = 0
+
+        def _resp_checker(resp, try_, _tries, *args, **kwds):
+            # If the VIOS is busy, then retry
+            return resp == 'VIOS IS BUSY'
+
+        @pvm_retry.retry(tries=4, http_codes=pvm_retry.RETRY_CODES,
+                         resp_checker=_resp_checker)
+        def _powervm_update(parm):
+            global called_count
+            called_count += 1
+            if called_count == 1:
+                # etag mismatch
+                resp = adpt.Response('reqmethod', 'reqpath', 412,
+                                     'reason', 'headers')
+                http_exc = pvm_exc.HttpError('msg', resp)
+                raise http_exc
+
+            if called_count == 2:
+                # Pretend we got a valid response, but the VIOS is busy
+                return 'VIOS IS BUSY'
+
+            if called_count == 3:
+                # Pretend we got a good response
+                return parm
+
+            return None
+
+        self.assertEqual(_powervm_update('Req'), 'Req')
+        self.assertEqual(called_count, 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ pypowervm/utils/retry.py
@@ -0,0 +1,103 @@
+# Copyright 2015 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import functools
+
+from six import moves
+
+from pypowervm import exceptions as exc
+
+# We can retry etag and busy errors
+HTTP_STATUS_ETAG_MISMATCH = 412
+
+RETRY_CODES = (HTTP_STATUS_ETAG_MISMATCH,)
+
+
+def no_delay(try_, tries, *args, **kwds):
+    pass
+
+
+def retry(tries=3, delay_func=no_delay,
+          retry_except=None, http_codes=None, test_func=None,
+          resp_checker=None, limit_except=None):
+    '''Retry method decorator
+
+    :param tries: The max number of calls to the wrapped method.
+    :param delay_func: A method to delay before retrying.
+    :param retry_except: A list of exceptions to retry if received.
+    :param http_codes: A list of http response codes to retry if received.
+    :param test_func: A method to call to determine whether to retry.
+    :param resp_checker: A method to call when no exception is caught, to
+        check the response and determine whether to retry.
+    :param limit_except: An exception to raise if the number of tries is
+        exhausted.
+    :returns: The return value of the wrapped method.
+
+    '''
+    def _retry(func):
+        @functools.wraps(func)
+        def __retry(*args, **kwds):
+            def test_retry(e):
+                if (not _test_func(e, try_, _tries, *args, **kwds) or
+                        try_ == _tries):
+                    if _limit_except:
+                        raise _limit_except
+                    else:
+                        raise
+                return
+
+            # Standardize input
+            # For some reason, if we use the parms in an 'if' directly
+            # python throws an exception.  Assigning them avoids it.
+            _tries = tries
+            _retry_except = retry_except
+            _http_codes = http_codes
+            _test_func = test_func
+            _resp_checker = resp_checker
+            _limit_except = limit_except
+            if _retry_except is None:
+                _retry_except = ()
+            if _http_codes is None:
+                _http_codes = ()
+            if _test_func is None:
+                caller_func = False
+                _test_func = lambda *args, **kwds: True
+            else:
+                caller_func = True
+            if _resp_checker is None:
+                _resp_checker = lambda *args, **kwds: True
+            # Start retries
+            for try_ in moves.range(1, _tries+1):
+                try:
+                    resp = func(*args, **kwds)
+                    # No exception raised, call the response checker
+                    # If we're on the last iteration, we return the response
+                    # the response checker should raise an exception if
+                    # it doesn't want this behavior.
+                    if (not _resp_checker(resp, try_, _tries, *args, **kwds)
+                            or try_ == _tries):
+                        return resp
+                except exc.HttpError as e:
+                    if caller_func or e.response.status in _http_codes:
+                        test_retry(e)
+                    else:
+                        raise
+                except _retry_except as e:
+                        test_retry(e)
+                # If we get here then we're going to retry
+                delay_func(try_, _tries, *args, **kwds)
+        return __retry
+    return _retry
