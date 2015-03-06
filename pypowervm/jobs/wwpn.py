7120f7d46d2ccd983a40a6915c9e5f53b92ee6b5
Drew Thorstensen <thorst@us.ibm.com>
2015-03-06 13:48:24 -0600

    Provide WWPN generation
    Some tools will need to generate the WWPNs for client VirtualFCAdapters.
    This is usually done during the Adapter creation.  However some
    scenarios may want to generate this ahead of time.
    
    This change set provides an API to do this.  Currently it provides a
    random generation method.  It is planned to support a globally unique
    WWPN naming scheme in a future change set.
    
    The liklihood of WWPN overlap is 1 in 281 trillion.  Within a given SAN
    fabric the liklihood of overlap is virtually non-existant.  Nonetheless,
    the future change set should resolve the remaining probability of
    overlap.
    
    This change set has a minor update to close_vterm to remove a 'no new
    line at end of file' warning.
    
    Change-Id: I144556ab145268eab16eb7dc0effa104f8bfe3fe

---
+++ pypowervm/jobs/wwpn.py
@@ -0,0 +1,34 @@
+# Copyright 2015 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import random
+
+
+def build_wwpn_pair(adapter, host_uuid):
+    """Builds a WWPN pair that can be used for a VirtualFCAdapter.
+
+    TODO(IBM): Future implementation will interrogate the system for globally
+               unique WWPN.  For now, generate based off of random number
+               generation.  Likelihood of overlap is 1 in 281 trillion.
+
+    :param adapter: The adapter to talk over the API.
+    :param host_uuid: The host system for the generation.
+    :return: Non-mutable WWPN Pair (set)
+    """
+    resp = "C0"
+    while len(resp) < 14:
+        resp += random.choice('0123456789ABCDEF')
+    return (resp + "00", resp + "01")
---
+++ pypowervm/tests/jobs/test_wwpn.py
@@ -0,0 +1,38 @@
+# Copyright 2015 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import unittest
+
+from pypowervm.jobs import wwpn
+
+
+class TestWWPN(unittest.TestCase):
+
+    def setUp(self):
+        super(TestWWPN, self).setUp()
+
+    def test_build_wwpn_pair(self):
+        # By its nature, this is a random generation algorithm.  Run it
+        # several times...just to increase probability of issues.
+        i = 0
+        while i < 100:
+            wwpn_pair = wwpn.build_wwpn_pair(None, None)
+            self.assertIsNotNone(wwpn_pair)
+            self.assertEqual(2, len(wwpn_pair))
+            for elem in wwpn_pair:
+                self.assertEqual(16, len(elem))
+                int(elem, 16)  # Would throw ValueError if not hex.
+            i += 1
