5aa926027092a4b6889e056ae8171f4d109b7dca
Eric Fried <efried@us.ibm.com>
2015-03-20 15:11:14 -0500

    Refactor upload/storage tasks into tasks.storage
    In preparation for adding more storage tasks (around PVs, VOpts, etc.)
    and factoring crt_vdisk out of upload_new_vdisk, it made sense for all
    the pieces relating to storage components, including uploading content
    to those storage components, to live in the same module.
    
    o pypowervm.tasks.upload_lv renamed to pypowervm.tasks.storage
    o crt_lu and rm_lu moved from tasks.cluster_ssp to tasks.storage
    
    Change-Id: Ib811f1d7848fbfde2a302a7c85f3e7fd4dd09df8

--- pypowervm/tests/tasks/test_cluster_ssp.py
+++ pypowervm/tests/tasks/test_cluster_ssp.py
@@ -14,10 +14,11 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import unittest
+
 import mock
 
 import pypowervm.adapter as adp
-import pypowervm.exceptions as exc
 import pypowervm.tasks.cluster_ssp as cs
 import pypowervm.tests.tasks.util as tju
 import pypowervm.util as u
@@ -26,21 +27,7 @@ import pypowervm.wrappers.constants as wc
 import pypowervm.wrappers.job as jwrap
 import pypowervm.wrappers.storage as stor
 
-import unittest
-
 CREATE_CLUSTER = 'cluster_create_job_template.txt'
-
-
-def _mock_update_by_path(ssp, etag, path):
-    # Spoof adding UDID and defaulting thinness
-    for lu in ssp.logical_units:
-        if not lu.udid:
-            lu._udid('udid_' + lu.name)
-        if lu.is_thin is None:
-            lu._is_thin(True)
-    resp = adp.Response('meth', 'path', 200, 'reason', {'etag': 'after'})
-    resp.entry = ssp.entry
-    return resp
 
 
 class TestClusterSSP(unittest.TestCase):
@@ -107,75 +94,6 @@ class TestClusterSSP(unittest.TestCase):
                            node, data)
         # run_job() should run delete_job() at the end
         self.assertEqual(mock_del_job.call_count, 1)
-
-
-class TestSSP(unittest.TestCase):
-
-    def setUp(self):
-        self.adp = mock.patch('pypowervm.adapter.Adapter')
-        self.adp.update_by_path = _mock_update_by_path
-        self.adp.extend_path = lambda x, xag: x
-        self.ssp = stor.SSP.bld('ssp1', [])
-        for i in range(5):
-            lu = stor.LU.bld('lu%d' % i, i+1)
-            lu._udid('udid_' + lu.name)
-            self.ssp.logical_units.append(lu)
-        self.ssp.entry.properties = {
-            'links': {'SELF': ['/rest/api/uom/SharedStoragePool/123']}}
-        self.ssp._etag = 'before'
-
-    def test_crt_lu(self):
-        ssp, lu = cs.crt_lu(self.adp, self.ssp, 'lu5', 10)
-        self.assertEqual(lu.name, 'lu5')
-        self.assertEqual(lu.udid, 'udid_lu5')
-        self.assertTrue(lu.is_thin)
-        self.assertEqual(ssp.etag, 'after')
-        self.assertIn(lu, ssp.logical_units)
-
-    def test_crt_lu_thin(self):
-        ssp, lu = cs.crt_lu(self.adp, self.ssp, 'lu5', 10, thin=True)
-        self.assertTrue(lu.is_thin)
-
-    def test_crt_lu_thick(self):
-        ssp, lu = cs.crt_lu(self.adp, self.ssp, 'lu5', 10, thin=False)
-        self.assertFalse(lu.is_thin)
-
-    def test_crt_lu_name_conflict(self):
-        self.assertRaises(exc.DuplicateLUNameError, cs.crt_lu, self.adp,
-                          self.ssp, 'lu1', 5)
-
-    def test_rm_lu_by_lu(self):
-        lu = self.ssp.logical_units[2]
-        ssp, lurm = cs.rm_lu(self.adp, self.ssp, lu=lu)
-        self.assertEqual(lu, lurm)
-        self.assertEqual(ssp.etag, 'after')
-        self.assertEqual(len(ssp.logical_units), 4)
-
-    def test_rm_lu_by_name(self):
-        lu = self.ssp.logical_units[2]
-        ssp, lurm = cs.rm_lu(self.adp, self.ssp, name='lu2')
-        self.assertEqual(lu, lurm)
-        self.assertEqual(ssp.etag, 'after')
-        self.assertEqual(len(ssp.logical_units), 4)
-
-    def test_rm_lu_by_udid(self):
-        lu = self.ssp.logical_units[2]
-        ssp, lurm = cs.rm_lu(self.adp, self.ssp, udid='udid_lu2')
-        self.assertEqual(lu, lurm)
-        self.assertEqual(ssp.etag, 'after')
-        self.assertEqual(len(ssp.logical_units), 4)
-
-    def test_rm_lu_not_found(self):
-        # By LU
-        lu = stor.LU.bld('lu5', 6)
-        self.assertRaises(exc.LUNotFoundError, cs.rm_lu, self.adp, self.ssp,
-                          lu=lu)
-        # By name
-        self.assertRaises(exc.LUNotFoundError, cs.rm_lu, self.adp, self.ssp,
-                          name='lu5')
-        # By UDID
-        self.assertRaises(exc.LUNotFoundError, cs.rm_lu, self.adp, self.ssp,
-                          udid='lu5_udid')
 
 if __name__ == '__main__':
     unittest.main()
