2a4dca1083e185b338aa0f7668881643c7c9ffdc
Kyle L. Henderson <kyleh@us.ibm.com>
2015-03-25 13:27:38 -0500

    Fix adapter read when not using caching
    In the Adapter's read_by_path method, the _locked_refresh method
    was always being used whether caching was requested or not. The
    effect was that in cases where reads of the same path happened
    within the refresh interval, it tried to access the cache,
    which was not setup and thus caused an exception.
    
    This change avoids performing the read operation through the
    _locked_refresh method when caching is not requested.
    
    Change-Id: I71444cb3f1dcc887e07e0410b36bb0491d36a4dd

--- pypowervm/adapter.py
+++ pypowervm/adapter.py
@@ -696,8 +696,16 @@ class Adapter(object):
                     etag = cached_resp.etag
                     etag_from_cache = True
         if not resp:
-            resp = _locked_refresh(dt.datetime.now(), cached_resp, etag,
-                                   etag_from_cache)
+            # If caching in use, then do the request in series
+            if self._cache:
+                resp = _locked_refresh(dt.datetime.now(), cached_resp, etag,
+                                       etag_from_cache)
+            else:
+                resp = self._read_by_path(path, etag, timeout, auditmemento,
+                                          sensitive, helpers=helpers)
+                if 'atom' in resp.reqheaders['Accept']:
+                    resp._unmarshal_atom()
+
         return resp
 
     def _read_by_path(self, path, etag, timeout, auditmemento, sensitive,
