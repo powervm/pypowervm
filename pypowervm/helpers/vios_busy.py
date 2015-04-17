f4eb11fae75d30a490d7a11a63eb22890dbcea42
Kyle L. Henderson <kyleh@us.ibm.com>
2015-04-17 14:17:10 -0500

    Add a wrapper for HttpErrorResponse
    Added an entry wrapper for HttpErrorResponse.
    Added test cases for the wrapper.
    Added a VIOS retry helper for retrying when the VIOS reports
    busy.
    
    Change-Id: I944192d34f430f0a07c0d8690fbfabff8bba4f4f

---
+++ pypowervm/helpers/vios_busy.py
@@ -0,0 +1,59 @@
+# Copyright 2014, 2015 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import time
+
+import pypowervm.exceptions as pvmex
+import pypowervm.wrappers.entry_wrapper as ew
+import pypowervm.wrappers.http_error as he
+
+# Make UT a little easier
+SLEEP = time.sleep
+
+
+def vios_busy_retry_helper(func, max_retries=3, delay=5):
+    """This helper retries the request if the resource is busy.
+
+    Args:
+        func: The Adapter request method to call
+        max_retries (int): Max number retries.
+    """
+    def wrapper(*args, **kwds):
+        retries = 0
+        while True:
+            try:
+                # Call the request()
+                resp = func(*args, **kwds)
+            except pvmex.Error as e:
+                resp = e.response
+                # See if the system was busy
+
+                if resp.body and resp.entry:
+                    wrap = ew.EntryWrapper.wrap(resp.entry)
+                    if (isinstance(wrap, he.HttpError) and
+                            wrap.is_vios_busy()):
+                        retries += 1
+                        if retries <= max_retries:
+                            # Wait a few seconds before trying again, scaling
+                            # out the delay based on the retry count.
+                            SLEEP(delay * retries)
+                            continue
+                # Doesn't look like a VIOS busy error, so just raise it.
+                raise
+            else:
+                return resp
+
+    return wrapper
---
+++ pypowervm/wrappers/http_error.py
@@ -0,0 +1,55 @@
+# Copyright 2015 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import pypowervm.const as pc
+import pypowervm.wrappers.entry_wrapper as ewrap
+
+import logging
+
+LOG = logging.getLogger(__name__)
+
+_REASON_CODE = 'ReasonCode'
+_MESSAGE = 'Message'
+_HTTP_STATUS = 'HTTPStatus'
+
+# Error codes that indicate the VIOS is busy
+_VIOS_BUSY_ERR_CODES = ['HSCL3205']
+
+
+@ewrap.EntryWrapper.pvm_type('HttpErrorResponse', ns=pc.WEB_NS)
+class HttpError(ewrap.EntryWrapper):
+
+    @property
+    def status(self):
+        return self._get_val_int(_HTTP_STATUS)
+
+    @property
+    def reason_code(self):
+        return self._get_val_str(_REASON_CODE)
+
+    @property
+    def message(self):
+        return self._get_val_str(_MESSAGE)
+
+    def is_vios_busy(self):
+        try:
+            msg = self.message
+            return (self.status == 500 and
+                    ('VIOS' in msg and
+                     'is busy processing some other request' in msg)
+                    or any(code in msg for code in _VIOS_BUSY_ERR_CODES))
+        except Exception:
+            return False
