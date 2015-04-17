a8b3ded452395c3e2fc5c37fc7e8f7463398d75b
Kyle L. Henderson <kyleh@us.ibm.com>
2015-04-17 14:17:10 -0500

    Add a wrapper for HttpErrorResponse
    Added an entry wrapper for HttpErrorResponse.
    Added test cases for the wrapper.
    Added a VIOS retry helper for retrying when the VIOS reports
    busy.
    
    Change-Id: I944192d34f430f0a07c0d8690fbfabff8bba4f4f

---
+++ pypowervm/tests/wrappers/test_http_error.py
@@ -0,0 +1,64 @@
+# Copyright 2015 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+import unittest
+
+from pypowervm.tests.wrappers.util import pvmhttp
+import pypowervm.wrappers.http_error as he
+
+HTTPRESP_FILE = "fake_httperror.txt"
+
+MSG = ('Unexpected error occurred while fetching Cluster/SSP '
+       'information : 9999-99Z*2125D4A/1 : Unable to send com'
+       'mand to VIOS at this moment. VIOS 1*9999-99Z*2125D4A '
+       'is busy processing some other request. Please retry t'
+       'he operation after sometime.')
+REASON_CODE = 'Unknown internal error.'
+
+
+class TestHttpError(unittest.TestCase):
+
+    def setUp(self):
+        super(TestHttpError, self).setUp()
+
+        self.http_error = pvmhttp.load_pvm_resp(HTTPRESP_FILE)
+        self.assertIsNotNone(self.http_error,
+                             "Could not load %s " %
+                             HTTPRESP_FILE)
+
+    def test_wrap(self):
+        wrap = he.HttpError.wrap(self.http_error.response.entry)
+        self.assertEqual(wrap.message, MSG)
+        self.assertEqual(wrap.status, 500)
+        self.assertEqual(wrap.reason_code, REASON_CODE)
+
+        self.assertTrue(wrap.is_vios_busy())
+        # Ensure it's checking for 500 only.
+        with mock.patch.object(wrap, '_get_val_int', return_value=555):
+            self.assertFalse(wrap.is_vios_busy())
+        # Ensure it's checking for 'VIOS' string.
+        with mock.patch.object(wrap, '_get_val_str', return_value='other'):
+            self.assertFalse(wrap.is_vios_busy())
+        # Ensure it finds 'VIOS' but not the other string.
+        with mock.patch.object(wrap, '_get_val_str', return_value='VIOS xxx'):
+            self.assertFalse(wrap.is_vios_busy())
+
+        # Ensure it finds 'HSCL' return code.
+        msg_string = 'msg HSCL3205 msg'
+        with mock.patch.object(wrap, '_get_val_str', return_value=msg_string):
+            self.assertTrue(wrap.is_vios_busy())
