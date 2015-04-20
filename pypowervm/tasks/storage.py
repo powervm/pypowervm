3588c26d6817d23d9f68ca8ad8d5a52cc53243db
Drew Thorstensen <thorst@us.ibm.com>
2015-04-20 12:37:19 -0500

    Support new file upload mechanism
    The REST API server supports a new 'local disk' file upload mechanism.
    If you're running on the same system as the REST API, instead of sending
    a file to the REST API for upload, one can instead simply write to a
    file pipe.
    
    This reduces the overhead on the REST API server dramatically (lower
    CPU, faster performance of the upload).
    
    This change set adds support for this flow.  Specific changes:
     - Add a new asset_file property to the vios_file wrapper.  This only
       shows up if using the new upload
     - Change the enumeration types to be more terse.
     - Add a new vio file enumeration type
     - Update the file upload for LU and vDisk to support this new
       mechanism.
    
    Change-Id: Icf8d91c9aaa242e34317a85c4ca31ddcde37a248

--- pypowervm/tasks/storage.py
+++ pypowervm/tasks/storage.py
@@ -16,7 +16,9 @@
 
 import logging
 import math
+import shutil
 
+from concurrent import futures
 from oslo_concurrency import lockutils as lock
 
 import pypowervm.const as c
@@ -82,11 +84,16 @@ def upload_new_vdisk(adapter, v_uuid,  vol_grp_uuid, d_stream,
 
     n_vdisk = crt_vdisk(adapter, v_uuid, vol_grp_uuid, d_name, gb_size)
 
+    # The file type.  If local API server, then we can use the coordinated
+    # file path.  Otherwise standard upload.
+    file_type = (vf.FileType.DISK_IMAGE_COORDINATED if adapter.traits.local_api
+                 else vf.FileType.DISK_IMAGE)
+
     # Next, create the file, but specify the appropriate disk udid from the
     # Virtual Disk
     vio_file = _create_file(
-        adapter, d_name, vf.FileType.BROKERED_DISK_IMAGE, v_uuid,
-        f_size=f_size, tdev_udid=n_vdisk.udid, sha_chksum=sha_chksum)
+        adapter, d_name, file_type, v_uuid, f_size=f_size,
+        tdev_udid=n_vdisk.udid, sha_chksum=sha_chksum)
 
     maybe_file = _upload_stream(adapter, vio_file, d_stream)
     return n_vdisk, maybe_file
@@ -116,8 +123,7 @@ def upload_vopt(adapter, v_uuid, d_stream, f_name, f_size=None,
     """
     # First step is to create the 'file' on the system.
     vio_file = _create_file(
-        adapter, f_name, vf.FileType.BROKERED_MEDIA_ISO, v_uuid,
-        sha_chksum, f_size)
+        adapter, f_name, vf.FileType.MEDIA_ISO, v_uuid, sha_chksum, f_size)
     f_uuid = _upload_stream(adapter, vio_file, d_stream)
 
     # Simply return a reference to this.
@@ -160,14 +166,18 @@ def upload_new_lu(adapter, v_uuid,  ssp, d_stream, lu_name, f_size,
         d_size = f_size
     gb_size = util.convert_bytes_to_gb(d_size, dp=2)
 
-    ssp, new_lu = crt_lu(adapter, ssp, lu_name, gb_size,
-                         typ=stor.LUType.IMAGE)
+    ssp, new_lu = crt_lu(adapter, ssp, lu_name, gb_size, typ=stor.LUType.IMAGE)
+
+    # The file type.  If local API server, then we can use the coordinated
+    # file path.  Otherwise standard upload.
+    file_type = (vf.FileType.DISK_IMAGE_COORDINATED if adapter.traits.local_api
+                 else vf.FileType.DISK_IMAGE)
 
     # Create the file, specifying the UDID from the new Logical Unit.
     # The File name matches the LU name.
     vio_file = _create_file(
-        adapter, lu_name, vf.FileType.BROKERED_DISK_IMAGE, v_uuid,
-        f_size=f_size, tdev_udid=new_lu.udid, sha_chksum=sha_chksum)
+        adapter, lu_name, file_type, v_uuid, f_size=f_size,
+        tdev_udid=new_lu.udid, sha_chksum=sha_chksum)
 
     maybe_file = _upload_stream(adapter, vio_file, d_stream)
     return new_lu, maybe_file
@@ -201,8 +211,37 @@ def _upload_stream(adapter, vio_file, d_stream):
              used to retry the cleanup.
     """
     try:
-        # Upload the file
-        adapter.upload_file(vio_file.element, d_stream)
+        if vio_file.enum_type == vf.FileType.DISK_IMAGE_COORDINATED:
+            # This path offers low CPU overhead and higher throughput, but
+            # can only be executed if running on the same system as the API.
+            # It works by writing to a file 'pipe'.  This is harder to
+            # coordinate.  But the vio_file's 'asset_file' tells us where
+            # to write the stream to.
+            #
+            # A reader to tell the API we have nothing to upload
+            class EmptyReader(object):
+                def read(self, size):
+                    return None
+
+            # Used for the threading.  As this is a blocking API.
+            def upload():
+                adapter.upload_file(vio_file.element, EmptyReader())
+
+            def stream():
+                with open(vio_file.asset_file, 'ab', 0) as out_stream:
+                    shutil.copyfileobj(d_stream, out_stream)
+
+            executor = futures.ThreadPoolExecutor(max_workers=2)
+            upload_f = executor.submit(upload)
+            stream_f = executor.submit(stream)
+
+            # Make sure we call the results.  This is just to make sure it
+            # doesn't have exceptions
+            upload_f.result()
+            stream_f.result()
+        else:
+            # Upload the file directly to the REST API server.
+            adapter.upload_file(vio_file.element, d_stream)
     finally:
         try:
             # Cleanup after the upload
