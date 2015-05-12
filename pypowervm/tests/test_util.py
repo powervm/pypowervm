4e215371baf1d7d70f4e37a929f10c8c2550abdf
Eric Fried <efried@us.ibm.com>
2015-05-12 17:10:23 -0500

    Util method to sanitize partition name
    The new method pypowervm.util.sanitize_partition_name_for_api accepts a
    name and returns it with any illegal characters replaced with
    underscores; and at a maximum length of 31 characters.  An optional
    parameter indicates that truncation is not allowed, resulting in
    ValueError if the input parameter is longer than 31 characters.
    
    Change-Id: I1997cf3bb2ee8e4d54604f5f9174af72463267db

--- pypowervm/tests/test_util.py
+++ pypowervm/tests/test_util.py
@@ -270,6 +270,32 @@ class TestUtil(unittest.TestCase):
                           prefix=allc, suffix=allc)
         self.assertRaises(ValueError, util.sanitize_file_name_for_api, '')
 
+    def test_sanitize_partition_name_for_api(self):
+        allc = ''.join(map(chr, range(256)))
+        self.assertEqual('foo', util.sanitize_partition_name_for_api('foo'))
+        self.assertEqual('_______________________________',
+                         util.sanitize_partition_name_for_api(allc))
+        self.assertEqual('_ !_#_%_____+,-./0123456789:;_=',
+                         util.sanitize_partition_name_for_api(allc[31:]))
+        self.assertEqual('__@ABCDEFGHIJKLMNOPQRSTUVWXYZ__',
+                         util.sanitize_partition_name_for_api(allc[62:]))
+        self.assertEqual('_^__abcdefghijklmnopqrstuvwxyz{',
+                         util.sanitize_partition_name_for_api(allc[93:]))
+        self.assertEqual('_}_____________________________',
+                         util.sanitize_partition_name_for_api(allc[124:]))
+        for start in (155, 186, 217):
+            self.assertEqual(
+                '_______________________________',
+                util.sanitize_partition_name_for_api(allc[start:]))
+        self.assertEqual('________',
+                         util.sanitize_partition_name_for_api(allc[248:]))
+
+        self.assertRaises(ValueError, util.sanitize_partition_name_for_api,
+                          allc, trunc_ok=False)
+        self.assertRaises(ValueError, util.sanitize_partition_name_for_api, '')
+        self.assertRaises(ValueError, util.sanitize_partition_name_for_api,
+                          None)
+
     # Tests for check_and_apply_xag covered by
     # test_adapter.TestAdapter.test_extended_path
 
