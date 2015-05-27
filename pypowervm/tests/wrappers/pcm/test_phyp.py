37063026f0d54e7a88cf07d46a3c3ee523318899
Drew Thorstensen <thorst@us.ibm.com>
2015-05-26 21:31:43 -0400

    Support PCM Data Sources
    This change set introduces the ability to parse through two types of
    PCM Long Term Metrics.  They are phyp and VIOS.
    
    The PHYP metrics represent the data sources from the hypervisor itself.
    Certain pieces of data come back here that need to be correlated to the
    Virtual I/O Server metrics (which contain other details).
    
    These two are typically tied together to form a complete picture of the
    metrics within the system.
    
    This first change set provides the 'raw data'.  A subsequent set of
    change sets will provide additional features such as finding the latest
    phyp and vios elements from the feeds, determining 'system' metrics,
    etc...
    
    But before those can be built up, the raw data is needed which this
    provides.
    
    Also includes tasks to deal with updating the LTM preferences and
    querying the data as it is non-standard.
    
    Change-Id: Iec347296b5762932377cc40f2592b119ebb1de2d

---
+++ pypowervm/tests/wrappers/pcm/test_phyp.py
@@ -0,0 +1,133 @@
+# Copyright 2015 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+import testtools
+
+from pypowervm.tests.wrappers.util import pvmhttp
+from pypowervm.wrappers.pcm import phyp as pcm_phyp
+
+
+class TestPhypLTM(testtools.TestCase):
+
+    def setUp(self):
+        super(TestPhypLTM, self).setUp()
+
+        dirname = os.path.dirname(__file__)
+        file_name = os.path.join(dirname, 'data', 'phyp_data.txt')
+        self.raw_json = pvmhttp.PVMFile(file_name).body
+
+    def test_parse(self):
+        info = pcm_phyp.PhypInfo(self.raw_json)
+        self.assertIsNotNone(info)
+
+        # Validate the info
+        self.assertEqual('1.3.0', info.info.version)
+        self.assertEqual('Raw', info.info.metric_type)
+        self.assertEqual('LTM', info.info.monitoring_type)
+        self.assertEqual('8247-22L*2125D4A', info.info.mtms)
+        self.assertEqual('dev-4', info.info.name)
+
+        # Validate some samples
+        sample = info.sample
+        self.assertEqual(806297258933149, sample.time_based_cycles)
+        self.assertEqual(0, sample.status)
+        self.assertEqual(u'2015-05-27T08:17:45+0000', sample.time_stamp)
+
+        # Firmware
+        self.assertEqual(58599310268,
+                         sample.system_firmware.utilized_proc_cycles)
+        self.assertEqual(4096, sample.system_firmware.assigned_mem)
+
+        # Processor
+        self.assertEqual(20, sample.processor.total_proc_units)
+        self.assertEqual(20, sample.processor.configurable_proc_units)
+        self.assertEqual(18.9, sample.processor.available_proc_units)
+        self.assertEqual(512000000, sample.processor.proc_cycles_per_sec)
+
+        # Memory
+        self.assertEqual(65536, sample.memory.total_mem)
+        self.assertEqual(32512, sample.memory.available_mem)
+        self.assertEqual(65536, sample.memory.configurable_mem)
+
+        # LPARs
+        self.assertEqual(5, len(sample.lpars))
+
+        # First LPAR shouldn't have network or storage (inactive)
+        bad_lpar = sample.lpars[0]
+        self.assertEqual(6, bad_lpar.id)
+        self.assertEqual('2545BCC5-BAE8-4414-AD49-EAFC2DEE2546', bad_lpar.uuid)
+        self.assertEqual('aixlinux', bad_lpar.type)
+        self.assertEqual('fkh4-99b8fdca-kyleh', bad_lpar.name)
+        self.assertEqual('Not Activated', bad_lpar.state)
+        self.assertEqual(100, bad_lpar.affinity_score)
+        self.assertIsNotNone(bad_lpar.memory)
+        self.assertIsNotNone(bad_lpar.processor)
+        self.assertEqual(None, bad_lpar.network)
+        self.assertEqual(None, bad_lpar.storage)
+
+        # Last LPAR should have network and storage
+        good_lpar = sample.lpars[4]
+
+        # VM Memory
+        self.assertEqual(20480, good_lpar.memory.logical_mem)
+        self.assertEqual(20480, good_lpar.memory.backed_physical_mem)
+
+        # VM Processor
+        self.assertEqual(0, good_lpar.processor.pool_id)
+        self.assertEqual('uncap', good_lpar.processor.mode)
+        self.assertEqual(4, good_lpar.processor.max_virt_procs)
+        self.assertEqual(.4, good_lpar.processor.max_proc_units)
+        self.assertEqual(128, good_lpar.processor.weight)
+        self.assertEqual(1765629232513,
+                         good_lpar.processor.entitled_proc_cycles)
+        self.assertEqual(264619289721,
+                         good_lpar.processor.util_cap_proc_cycles)
+        self.assertEqual(641419282, good_lpar.processor.util_uncap_proc_cycles)
+        self.assertEqual(0, good_lpar.processor.idle_proc_cycles)
+        self.assertEqual(0, good_lpar.processor.donated_proc_cycles)
+        self.assertEqual(0, good_lpar.processor.time_wait_dispatch)
+        self.assertEqual(160866895489, good_lpar.processor.total_instructions)
+        self.assertEqual(193139925064,
+                         good_lpar.processor.total_inst_exec_time)
+
+        # VM Vea
+        vea = good_lpar.network.veas[0]
+        self.assertEqual(2227, vea.vlan_id)
+        self.assertEqual(0, vea.vswitch_id)
+        self.assertEqual('U8247.22L.2125D4A-V2-C2', vea.physical_location)
+        self.assertEqual(True, vea.is_pvid)
+        self.assertEqual(0, vea.received_packets)
+        self.assertEqual(0, vea.sent_packets)
+        self.assertEqual(0, vea.dropped_packets)
+        self.assertEqual(0, vea.sent_bytes)
+        self.assertEqual(0, vea.received_bytes)
+        self.assertEqual(0, vea.received_physical_packets)
+        self.assertEqual(0, vea.sent_physical_packets)
+        self.assertEqual(0, vea.dropped_physical_packets)
+        self.assertEqual(0, vea.sent_physical_bytes)
+        self.assertEqual(0, vea.received_physical_bytes)
+
+        # TODO(thorst) Test SR-IOV
+
+        # VM storage
+        stor = good_lpar.storage.v_stor_adpts[0]
+        self.assertEqual('U8247.22L.2125D4A-V2-C3', stor.physical_location)
+        self.assertEqual(1, stor.vios_id)
+        self.assertEqual(1000, stor.vios_slot)
+
+        # TODO(thorst) Test vfc
---
+++ pypowervm/wrappers/pcm/vios.py
@@ -0,0 +1,196 @@
+# Copyright 2015 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Wrappers to parse the output of the PCM JSON data from VIOS."""
+
+import abc
+import json
+
+import six
+
+from pypowervm.wrappers import pcm
+
+
+class ViosInfo(object):
+    """Represents a monitor sample from the Virtual I/O Server monitor.
+
+    The VIOS PCM monitor JSON data can be parsed by this.  The base structure
+    is:
+    - ViosInfo
+      - Info
+      - ViosSample
+        - Memory
+        - Network
+          - NetworkAdpt (List)
+          - SharedEthernetAdapter (List)
+        - Storage
+          - PhysFCAdpt (List)
+            - VirtFCAdpt (List)
+          - PhysStorageAdpt (List)
+          - VirtStorageAdpt (List)
+          - SSPs (List)
+    """
+
+    def __init__(self, raw_json):
+        data = json.loads(raw_json)
+        systemUtil = data.get('systemUtil')
+        self.info = pcm.Info(systemUtil.get('utilInfo'))
+        self.sample = ViosSample(systemUtil.get('utilSample'))
+
+
+class ViosSample(object):
+
+    def __init__(self, utilSample):
+        self.time_stamp = utilSample.get('timeStamp')
+
+        # TODO(thorst) Evaluate with multi VIOS.
+        vios = utilSample.get('viosUtil')[0]
+        self.id = vios.get('id')
+        self.name = vios.get('name')
+
+        # Complex types
+        mem = vios.get('memory')
+        self.mem = Memory(mem) if mem else None
+
+        net = vios.get('network')
+        self.network = Network(net) if net else None
+
+        storage = vios.get('storage')
+        self.storage = Storage(storage) if storage else None
+
+
+class Memory(object):
+
+    def __init__(self, mem):
+        self.utilized_mem = mem.get('utilizedMem')
+
+
+class Network(object):
+    """The Network elements within the VIOS."""
+
+    def __init__(self, net):
+        self.adpts = [NetworkAdpt(x) for x in net.get('genericAdapters')]
+        self.seas = [SharedEthernetAdapter(x)
+                     for x in net.get('sharedAdapters')]
+
+
+class NetworkAdpt(object):
+    """Represents a Network Adapter on the system."""
+
+    def __init__(self, adpt):
+        self.id = adpt.get('id')
+        # Type: 'virtual' or 'physical' or 'sea' (if NetworkBridge)
+        self.type = adpt.get('type')
+        self.physical_location = adpt.get('physicalLocation')
+        self.received_packets = adpt.get('receivedPackets')
+        self.sent_packets = adpt.get('sentPackets')
+        self.dropped_packets = adpt.get('droppedPackets')
+        self.received_bytes = adpt.get('receivedBytes')
+        self.sent_bytes = adpt.get('sentBytes')
+
+
+class SharedEthernetAdapter(NetworkAdpt):
+    """Represents a Shared Ethernet Adapter on the VIOS."""
+
+    def __init__(self, bridge):
+        super(SharedEthernetAdapter, self).__init__(bridge)
+        self.bridged_adpts = bridge.get('bridgedAdapters')
+
+
+class Storage(object):
+    """Represents the storage elements on the VIOS."""
+
+    def __init__(self, storage):
+        fc_adpts = storage.get('fiberChannelAdapters')
+        self.fc_adpts = [PhysFCAdpt(x) for x in fc_adpts]
+
+        phys_adpts = storage.get('genericPhysicalAdapters')
+        self.phys_adpts = [PhysStorageAdpt(x) for x in phys_adpts]
+
+        virt_adpts = storage.get('genericVirtualAdapters')
+        self.virt_adpts = [VirtStorageAdpt(x) for x in virt_adpts]
+
+        ssps = storage.get('sharedStoragePools')
+        self.ssps = [SSP(x) for x in ssps]
+
+
+@six.add_metaclass(abc.ABCMeta)
+class StorageAdpt(object):
+    """Base class for storage adapters."""
+
+    def __init__(self, adpt):
+        self.id = adpt.get('id')
+        self.physical_location = adpt.get('physicalLocation')
+        self.num_reads = adpt.get('numOfReads')
+        self.num_writes = adpt.get('numOfWrites')
+        self.read_bytes = adpt.get('readBytes')
+        self.write_bytes = adpt.get('writeBytes')
+
+
+class PhysFCAdpt(StorageAdpt):
+    """Represents a physical fiber channel adapter on the VIOS."""
+
+    def __init__(self, adpt):
+        super(PhysFCAdpt, self).__init__(adpt)
+        self.wwpn = adpt.get('wwpn')
+
+        # Appears to be Gb/s interface speed
+        self.running_speed = adpt.get('runningSpeed')
+
+        # TODO(thorst) Add FC Ports (need vfc mappings)
+        vadpts = adpt.get('ports')
+        self.ports = [VirtFCAdpt(x) for x in vadpts]
+
+
+class VirtFCAdpt(StorageAdpt):
+    """Represents a Virtual FC Port (NPIV)."""
+
+    def __init__(self, vadpt):
+        super(VirtFCAdpt, self).__init__(vadpt)
+        self.wwpn = vadpt.get('wwpn')
+
+        # Appears to be Gb/s interface speed
+        self.running_speed = vadpt.get('runningSpeed')
+
+
+class PhysStorageAdpt(StorageAdpt):
+    """Represents a physical storage adapter (typically a SAS drive)."""
+
+    def __init__(self, adpt):
+        super(PhysStorageAdpt, self).__init__(adpt)
+        self.type = adpt.get('type')
+
+
+class VirtStorageAdpt(StorageAdpt):
+    """Represents a virtual storage adapter (vscsi)."""
+
+    def __init__(self, adpt):
+        super(VirtStorageAdpt, self).__init__(adpt)
+        self.type = adpt.get('type')
+
+
+class SSP(object):
+    """Represents a Shared Storage Pool (entire element)."""
+
+    def __init__(self, ssp):
+        self.id = ssp.get('id')
+        self.pool_disks = ssp.get('poolDisks')
+        self.num_reads = ssp.get('numOfReads')
+        self.num_writes = ssp.get('numOfWrites')
+        self.total_space = ssp.get('totalSpace')
+        self.used_space = ssp.get('usedSpace')
+        self.read_bytes = ssp.get('readBytes')
+        self.write_bytes = ssp.get('writeBytes')
