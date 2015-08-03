06053fd974fdd28293a41cb5b1b3cfc965bb9730
Kyle L. Henderson <kyleh@us.ibm.com>
2015-08-03 10:33:24 -0500

    Add management console wrapper
    Added support for management console wrapper and the unit test
    cases.
    
    Broke out MTMS element wrapper from the managed system module into
    it's own module and used it in managed system, cluster, and
    management console wrappers.
    
    Change-Id: I4f1d90d591ba8cd2b637977f1a3bf027c8f86d19

---
+++ pypowervm/tasks/management_console.py
@@ -0,0 +1,53 @@
+# Copyright 2015 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from pypowervm.utils import transaction as trans
+from pypowervm.wrappers import management_console as mc
+
+
+def get_public_key(adapter):
+    """Get the public key for the management console.
+
+    :param adapter: The adapter for the pypowervm API.
+    :return: The public key
+    """
+    # Get the consoles feed and use the first, there is just one.
+    console = mc.ManagementConsole.wrap(
+        adapter.read(mc.ManagementConsole.schema_type))[0]
+    return console.ssh_public_key
+
+
+def add_authorized_key(adapter, public_key):
+    """Add an authorized public key to the management console.
+
+    The public_key will be added if it doesn't already exist.
+
+    :param adapter: The adapter for the pypowervm API.
+    :param public_key: The public key to be added.
+    """
+    console_w = mc.ManagementConsole.wrap(
+        adapter.read(mc.ManagementConsole.schema_type))[0]
+
+    @trans.entry_transaction
+    def run_update(console):
+        keys = console.ssh_authorized_keys
+        if public_key not in keys:
+            keys = list(keys)
+            keys.append(public_key)
+            console.ssh_authorized_keys = keys
+            console.update()
+
+    run_update(console_w)
---
+++ pypowervm/wrappers/management_console.py
@@ -0,0 +1,130 @@
+# Copyright 2015 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Wrappers, constants, and helpers around ManagementConsole."""
+
+import pypowervm.const as c
+import pypowervm.util as u
+import pypowervm.wrappers.entry_wrapper as ewrap
+import pypowervm.wrappers.mtms as mtmwrap
+
+import logging
+
+LOG = logging.getLogger(__name__)
+
+_MGD_FRAMES = 'ManagedFrames'
+_MGD_SYS = 'ManagedSystems'
+_MGMT_CON_NAME = 'ManagementConsoleName'
+
+_MGND_SYS_LINK = u.xpath("ManagedSystems", c.LINK)
+
+# NETI XPath constants
+_NETI_ROOT = 'NetworkInterfaces'
+
+_MGMT_NETI_ROOT = 'ManagementConsoleNetworkInterface'
+_MGMT_NETI_NAME = 'InterfaceName'
+_MGMT_NETI_ADDRESS = 'NetworkAddress'
+
+_PWR_ENT_POOLS = 'PowerEnterprisePools'
+
+# SSH Config
+_PUB_KEY = 'PublicSSHKey'
+_AUTH_KEYS = 'AuthorizedKeys'
+_AUTH_KEY = 'AuthorizedKey'
+_KEY = 'Key'
+
+
+_CONS_EL_ORDER = (mtmwrap.MTMS_ROOT, _MGD_FRAMES, _MGD_SYS, _MGMT_CON_NAME,
+                  _NETI_ROOT, _PWR_ENT_POOLS, _PUB_KEY, _AUTH_KEYS)
+
+
+@ewrap.EntryWrapper.pvm_type('ManagementConsole', child_order=_CONS_EL_ORDER)
+class ManagementConsole(ewrap.EntryWrapper):
+
+    @property
+    def name(self):
+        return self._get_val_str(_MGMT_CON_NAME)
+
+    @property
+    def mtms(self):
+        return mtmwrap.MTMS.wrap(self.element.find(mtmwrap.MTMS_ROOT))
+
+    @property
+    def network_interfaces(self):
+        return NetworkInterfaces.wrap(self.element.find(_NETI_ROOT))
+
+    @property
+    def ssh_public_key(self):
+        return self._get_val_str(_PUB_KEY)
+
+    @property
+    def ssh_authorized_keys(self):
+        """Returns a list of keys.
+
+        The returned list contains the keys as plan strings.
+        """
+        return tuple(key_w.key for key_w in
+                     ewrap.WrapperElemList(self._find_or_seed(_AUTH_KEYS),
+                                           AuthorizedKey))
+
+    @ssh_authorized_keys.setter
+    def ssh_authorized_keys(self, keys):
+        """Sets the keys given a list of key strings."""
+        self.replace_list(
+            _AUTH_KEYS, [AuthorizedKey.bld(self.adapter, key) for key in keys],
+            attrib=c.ATTR_SCHEMA130)
+
+
+@ewrap.ElementWrapper.pvm_type(_AUTH_KEY, attrib={})
+class AuthorizedKey(ewrap.ElementWrapper):
+    """The Authorized Key wrapper."""
+
+    @classmethod
+    def bld(cls, adapter, key):
+        new_key = super(AuthorizedKey, cls)._bld(adapter)
+        new_key.key = key
+        return new_key
+
+    @property
+    def key(self):
+        return self.element.text
+
+    @key.setter
+    def key(self, val):
+        self.element.text = val
+
+
+@ewrap.ElementWrapper.pvm_type(_NETI_ROOT, has_metadata=True)
+class NetworkInterfaces(ewrap.ElementWrapper):
+    """The Network Interfaces wrapper."""
+
+    @property
+    def console_interface(self):
+        return ConsoleNetworkInterfaces.wrap(
+            self.element.find(_MGMT_NETI_ROOT))
+
+
+@ewrap.ElementWrapper.pvm_type(_MGMT_NETI_ROOT, has_metadata=True)
+class ConsoleNetworkInterfaces(ewrap.ElementWrapper):
+    """The Console Network Interfaces wrapper."""
+
+    @property
+    def name(self):
+        return self._get_val_str(_MGMT_NETI_NAME)
+
+    @property
+    def address(self):
+        return self._get_val_str(_MGMT_NETI_ADDRESS)
