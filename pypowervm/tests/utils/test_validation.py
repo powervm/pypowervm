7a44df2263b5adc3c62214555d3aec1ef14874c7
Matt Rabe <mdrabe@us.ibm.com>
2015-08-12 18:24:49 +0200

    Add validation utils for LPARWrappers
    This validation is meant to catch any errors the PowerVM
    management interface would throw during a deploy or resize
    operation.
    
    For now the validation being implemented is checking that
    the desired processing units in the LPARWrapper is available
    on the host.
    
    Change-Id: I19262f183a1cf66cf730cd0527aace257e4d2978

---
+++ pypowervm/tests/utils/test_validation.py
@@ -0,0 +1,61 @@
+# Copyright 2015 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+import testtools
+
+from pypowervm.utils import validation as vldn
+from pypowervm.wrappers import logical_partition as lpar
+from pypowervm.wrappers import managed_system as mgd_sys
+
+
+class TestValidator(testtools.TestCase):
+    """Unit tests for validation."""
+
+    def setUp(self):
+        super(TestValidator, self).setUp()
+
+        def _bld_mgd_sys(proc_units_avail, mem_free):
+            # Build a fake managed system wrapper
+            mngd_sys = mock.MagicMock()
+            mngd_sys.proc_units_avail = proc_units_avail
+            mngd_sys.memory_free = mem_free
+            mocked = mock.MagicMock(spec=mgd_sys.System, return_value=mngd_sys)
+            return mocked()
+
+        def _bld_lpar(proc_units, des_mem, has_dedicated=False,
+                      name='default'):
+            lpar_w = mock.MagicMock()
+            lpar_w.proc_config.has_dedicated = has_dedicated
+            lpar_w.name = name
+            if has_dedicated:
+                lpar_w.proc_config.dedicated_proc_cfg.desired = proc_units
+            else:
+                lpar_w.proc_config.shared_proc_cfg.desired_units = proc_units
+            mocked = mock.MagicMock(spec_set=lpar.LPAR, return_value=lpar_w)
+            return mocked()
+
+        self.mngd_sys = _bld_mgd_sys(20.0, 32768)
+        self.lpar_21_procs = _bld_lpar(21.0, 2048, name='lpar_21_procs')
+        self.lpar_1_proc = _bld_lpar(1.0, 2048, name='lpar_1_proc')
+
+    def test_validator(self):
+        # Test desired proc units > host avail proc units fails
+        vldr = vldn.LPARWrapperValidator(self.lpar_21_procs, self.mngd_sys)
+        self.assertRaises(vldn.ValidatorException, vldr.validate_all)
+
+        # Test desired proc units < host avail proc units passes
+        vldn.LPARWrapperValidator(self.lpar_1_proc, self.mngd_sys)
---
+++ pypowervm/utils/validation.py
@@ -0,0 +1,141 @@
+# Copyright 2015 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Extended validation utilities."""
+
+import abc
+import logging
+import six
+
+from pypowervm.i18n import _
+
+LOG = logging.getLogger(__name__)
+
+
+class ValidatorException(Exception):
+    """Exceptions thrown from the validators."""
+    pass
+
+
+class LPARWrapperValidator(object):
+    """LPAR Validator.
+
+    This class implements additional validation for LPARs for use
+    during resize or deployment.
+
+    It is meant to catch any violations that would cause errors at
+    the PowerVM management interface.
+
+    """
+    def __init__(self, lpar_w, sys_w, curr_lpar_w=None):
+        """Initialize the validator
+
+        :param lpar_w: LPAR wrapper intended to be validated
+        :param sys_w: managed_system wrapper intended to be validated
+            against such as making sure the host has the desired resources
+            of the LPAR available.
+        :param curr_lpar_w: (Optional) current LPAR wrapper used to validate
+            deltas during a resize operation. If this is passed in then it
+            assumes resize validation.
+        """
+        self.lpar_w = lpar_w
+        self.sys_w = sys_w
+        self.curr_lpar_w = curr_lpar_w
+
+    def validate_all(self):
+        """Invoke attribute validation classes to perform validation"""
+        ProcValidator(self.lpar_w, self.sys_w, self.curr_lpar_w).validate()
+
+
+@six.add_metaclass(abc.ABCMeta)
+class BaseValidator(object):
+    """Base Validator.
+
+    This class is responsible for delegating validation depending on
+    if it's a deploy, active resize, or inactive resize.
+
+    """
+    def __init__(self, lpar_w, sys_w, curr_lpar_w=None):
+        """Initialize LPAR and System Wrappers."""
+        self.lpar_w = lpar_w
+        self.sys_w = sys_w
+        self.curr_lpar_w = curr_lpar_w
+
+    def validate(self):
+        """Determines what validation is needed and invokes it."""
+        # Deploy
+        if self.curr_lpar_w is None:
+            self._validate_deploy()
+        # Inactive Resize
+        # TODO(IBM):
+
+        # Active Resize
+        # TODO(IBM):
+
+    @abc.abstractmethod
+    def _validate_deploy(self):
+        """Deploy validation abstract method."""
+
+    @abc.abstractmethod
+    def _validate_active_resize(self):
+        """Active resize validation abstract method."""
+
+    @abc.abstractmethod
+    def _validate_inactive_resize(self):
+        """Inactive resize validation abstract method."""
+
+    def _validate_host_has_available_res(self, des, avail, res_name):
+        if round(des, 2) > round(avail, 2):
+            ex_args = {'requested': '%.2f' % des,
+                       'avail': '%.2f' % avail,
+                       'instance_name': self.lpar_w.name,
+                       'res_name': res_name}
+            msg = _("Insufficient available %(res_name)s on host for virtual "
+                    "machine '%(instance_name)s' (%(requested)s "
+                    "requested, %(avail)s available)") % ex_args
+            LOG.error(msg)
+            raise ValidatorException(msg)
+
+
+class ProcValidator(BaseValidator):
+
+    def _validate_deploy(self):
+        """Validate processor values for deployment.
+
+        Validation logic in place
+        1. desired processors are available on host
+        """
+        procs_avail = self.sys_w.proc_units_avail
+        if self.lpar_w.proc_config.has_dedicated:
+            des_procs = (self.lpar_w.proc_config.
+                         dedicated_proc_cfg.desired)
+            self._validate_host_has_available_res(des_procs,
+                                                  procs_avail,
+                                                  _('CPUs'))
+        else:
+            des_procs = (self.lpar_w.proc_config.
+                         shared_proc_cfg.desired_units)
+            self._validate_host_has_available_res(des_procs,
+                                                  procs_avail,
+                                                  _('processing units'))
+
+    def _validate_active_resize(self):
+        # TODO(IBM):
+        pass
+
+    def _validate_inactive_resize(self):
+        # TODO(IBM):
+        pass
