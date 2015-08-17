5b328a5c2a325c06901f3740a9b9d289deac3ca6
Drew Thorstensen <thorst@us.ibm.com>
2015-08-17 16:45:49 -0400

    Order dependence in the client adapter wwpns
    The order of the WWPNs is important when interacting with the pypowervm
    API.  This was originally not thought to be the case, but this is
    important as it dictates the order for how the client WWPNs become
    active.
    
    This change set removes the 'set' logic and instead uses 'lists' which
    maintain order.
    
    Change-Id: I2921d10af85a8cabea9856f38412f9640a4811ee
    Closes-Bug: 1485738

--- pypowervm/wrappers/storage.py
+++ pypowervm/wrappers/storage.py
@@ -810,19 +810,19 @@ class VFCClientAdapter(VClientStorageAdapter):
     def _wwpns(self, value):
         """Sets the WWPN string.
 
-        :param value: The set (or list) of WWPNs.  Should only contain two.
+        :param value: The list of WWPNs.  Should only contain two.
         """
         if value is not None:
             self.set_parm_value(_VADPT_WWPNS, " ".join(value).lower())
 
     @property
     def wwpns(self):
-        """Returns a set that contains the WWPNs.  If no WWPNs, empty set."""
+        """Returns a list that contains the WWPNs.  If no WWPNs, empty list."""
         val = self._get_val_str(_VADPT_WWPNS)
         if val is None:
-            return set()
+            return []
         else:
-            return set(val.upper().split(' '))
+            return val.upper().split(' ')
 
 
 # pvm_type decorator by superclass (it is not unique)
