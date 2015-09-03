bb7941dca53aae020722893f96891f78ced1a670
chhagarw <chhagarw@in.ibm.com>
2015-09-03 14:55:33 -0400

    LUA good discovery conditions
    Adding the function to verify the LUA discovery success conditions.
    Added DEVICE_IN_USE as a success LUA discovery status.
    This method can then be used anywhere to check good discovery.
    
    Change-Id: I52caa68d9a2abee26ac6ea421dad950370dab6d4

--- pypowervm/tasks/hdisk.py
+++ pypowervm/tasks/hdisk.py
@@ -100,6 +100,22 @@ class ITL(object):
         return not self.__eq__(other)
 
 
+def good_discovery(status, device_name):
+    """Checks the hdisk discovery results for a good discovery.
+
+    Acceptable LUA discovery statuses are :-
+    DEVICE_AVAILABLE: hdisk discovered on all the ITL paths and available.
+    DEVICE_IN_USE: hdisk discovered on all the ITL paths and is in-use by
+    the server.
+    FOUND_ITL_ERR: hdisk is discovered on some of the ITL paths and available.
+    This can happen if there are multiple ITL nexus paths are passed. If hdisk
+    can be discovered on any of the paths its considered as success.
+   """
+    return device_name is not None and status in [
+        LUAStatus.DEVICE_AVAILABLE, LUAStatus.DEVICE_IN_USE,
+        LUAStatus.FOUND_ITL_ERR]
+
+
 def build_itls(i_wwpns, t_wwpns, lun):
     """This method builds the list of ITLs for all of the permutations.
 
@@ -153,8 +169,7 @@ def discover_hdisk(adapter, vios_uuid, itls, vendor=LUAType.OTHER):
     status, devname, udid = lua_recovery(adapter, vios_uuid, itls,
                                          vendor=vendor)
     # Do we need to scrub and retry?
-    if devname is None or status not in (LUAStatus.DEVICE_AVAILABLE,
-                                         LUAStatus.FOUND_ITL_ERR):
+    if not good_discovery(status, devname):
         vwrap = pvm_vios.VIOS.getter(adapter, entry_uuid=vios_uuid,
                                      xag=(pvm_vios.VIOS.xags.SCSI_MAPPING,
                                           pvm_vios.VIOS.xags.FC_MAPPING)).get()
