dbaab8aececcdedef849265661c3fc02f5c06900
Eric Fried <efried@us.ibm.com>
2015-12-09 09:43:58 -0600

    Prevent element poaching when creating new Element
    A quirk of lxml's implementation of ElementTree is that adding an
    element of one XML hierarchy to a different XML hierarchy *removes* that
    element from the first hierarchy.  See the fourth bullet here:
    http://lxml.de/compatibility.html
    Pseudocode example:
    
    > len(dom1.children)
    3
    > dom2.append(dom1.children[0])
    > len(dom1.children)
    2
    
    pypowervm was being bitten by this quirk: when creating or augmenting a
    pypowervm.entities.Element, if a caller passed in one or more member(s)
    of some other hierarchy as children, those elements would disappear from
    the source hierarchy.
    
    This change set resolves the issue on Element.__init__ by deep-copying
    the children before adding them to the destination Element.  (We
    couldn't figure out how to fix Element.append, so that method got a
    docstring add and a TODO.)
    
    Change-Id: I05a2aaa4599e9f1e2e63ea749eb0ea5b3884e705

--- pypowervm/entities.py
+++ pypowervm/entities.py
@@ -214,7 +214,11 @@ class Element(object):
         if text:
             self.element.text = etree.CDATA(text) if cdata else text
         for c in children:
-            self.element.append(c.element)
+            # Use a deep copy, else, c.element gets *removed* from its parent
+            # hierarchy (see fourth bullet: http://lxml.de/compatibility.html).
+            # Doing the deepcopy here means the caller doesn't have to worry
+            # about it.
+            self.element.append(copy.deepcopy(c.element))
         self.adapter = adapter
 
     def __len__(self):
@@ -361,7 +365,17 @@ class Element(object):
         self.element.set(key, value)
 
     def append(self, subelement):
-        """Adds subelement to the end of this element's list of subelements."""
+        """Adds subelement to the end of this element's list of subelements.
+
+        Note: if subelement is a reference to an element within another XML
+        hierarchy, it will be *removed* from that hierarchy.  If you intend to
+        reuse the parent object, you should pass a copy.deepcopy of the
+        subelement to this method.
+        """
+        # TODO(IBM): We *should* deepcopy to prevent child poaching (see fourth
+        # bullet here: http://lxml.de/compatibility.html) - but this breaks the
+        # world.  Figure out why, and fix it.
+        # self.element.append(copy.deepcopy(subelement.element))
         self.element.append(subelement.element)
 
     def inject(self, subelement, ordering_list=(), replace=True):
