6ca0070b175cc3565a53186823345d8e0533b628
zhuljbj <zhuljbj@cn.ibm.com>
2015-12-09 23:24:20 -0500

    Check the destination host is capable for IBMi VM LPM.
    To serve as a destination host of IBM i VM LPM, the host has to enable
    IBMi mobility capable. The change set is to check those capabilities
    before migrate the VM.
    
    Change-Id: I3b08b1ccfdcf52afa785f2f529761179ace6cc83

--- pypowervm/tests/wrappers/test_managed_system.py
+++ pypowervm/tests/wrappers/test_managed_system.py
@@ -244,6 +244,31 @@ class TestMSEntryWrapper(unittest.TestCase):
     def test_session_is_master(self):
         self.assertTrue(self.wrapper.session_is_master)
 
+    def test_migration_data(self):
+        expected_data = {'active_lpar_mobility_capable': True,
+                         'inactive_lpar_mobility_capable': True,
+                         'ibmi_lpar_mobility_capable': True,
+                         'custom_mac_addr_capable': True,
+                         'ibmi_restrictedio_capable': True,
+                         'simplified_remote_restart_capable': False,
+                         'aix_capable': False,
+                         'ibmi_capable': True,
+                         'linux_capable': False,
+                         'shared_processor_pool_capable': True,
+                         'max_migration_ops_supported': 9,
+                         'active_migrations_supported': 0,
+                         'inactive_migrations_supported': 5,
+                         'preferred_active_migrations_supported': 0,
+                         'preferred_inactive_migrations_supported': 5,
+                         'active_migrations_in_progress': 0,
+                         'inactive_migrations_in_progress': 0,
+                         'proc_compat': 'default,POWER5,POWER6,'
+                         'POWER6_Enhanced,POWER6_Plus_Enhanced,POWER7'
+                         }
+        result_data = self.wrapper.migration_data
+        self.assertEqual(result_data, expected_data,
+                         'The returned data did not match expected values')
+
 
 class TestMTMS(unittest.TestCase):
     def test_mtms(self):
