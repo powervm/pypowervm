81aa9cdefafd74bad196abaa626ef107ede934e9
Chris Bush <clbush@us.ibm.com>
2016-01-05 12:38:47 -0600

    Add loc_code and vios_id to TrunkAdapter wrapper
    The LocationCode is an element on the xml payload for a trunk
    adapter, and the partition id on a Trunk Adapter can be inferred
    by its location code. All location codes take the following form:
    
    <MachineType>.<Model>.<Serial>-V<PartID>-C<SlotNumber>
    
    Using regex to parse out the partition ID.
    
    Change-Id: I0f9921c38c9febfeb2d0a368a3994c1c32529093

--- pypowervm/util.py
+++ pypowervm/util.py
@@ -459,3 +459,15 @@ def find_wrapper(haystack, needle_uuid):
 def xpath(*toks):
     """Constructs an XPath out of the passed-in string components."""
     return XPATH_DELIM.join(toks)
+
+
+def part_id_by_loc_code(loc_code):
+    """Takes a location code and returns its associated partition short ID.
+
+    All location codes on a trunk adapter are of the form:
+    <MachineType>.<Model>.<Serial>-V<PartID>-C<SlotNumber>
+
+    :return: An int of the associated partition short ID.
+    """
+    id_match = re.search('.*-V(.+?)-.*', loc_code)
+    return int(id_match.group(1)) if id_match else None
