63f1d0ae13d1a1caec316a5e69ada019581a77d3
Eric Fried <efried@us.ibm.com>
2016-02-19 12:18:17 -0600

    Support renaming SSP Logical Units
    REST/VIOS support renaming SSP LUs.  Add this support through pypowervm
    via a public setter @property for pypowervm.wrappers.storage.LU.name.
    
    Change-Id: I6b18c75b60616a1c79a82125719d203e3f5b8674

--- pypowervm/wrappers/storage.py
+++ pypowervm/wrappers/storage.py
@@ -569,7 +569,7 @@ class LU(ewrap.ElementWrapper):
                  prior to update.
         """
         lu = super(LU, cls)._bld(adapter)
-        lu._name(name)
+        lu.name = name
         lu._capacity(capacity)
         if thin is not None:
             lu._is_thin(thin)
@@ -589,7 +589,7 @@ class LU(ewrap.ElementWrapper):
         :returns: An Element that can be used for a PhysicalVolume create.
         """
         lu = super(LU, cls)._bld(adapter)
-        lu._name(name)
+        lu.name = name
         lu._udid(udid)
         return lu
 
@@ -619,15 +619,16 @@ class LU(ewrap.ElementWrapper):
     def name(self):
         return self._get_val_str(_LU_NAME)
 
-    def _name(self, value):
-        return self.set_parm_value(_LU_NAME, value)
+    @name.setter
+    def name(self, value):
+        self.set_parm_value(_LU_NAME, value)
 
     @property
     def udid(self):
         return self._get_val_str(_LU_UDID)
 
     def _udid(self, value):
-        return self.set_parm_value(_LU_UDID, value)
+        self.set_parm_value(_LU_UDID, value)
 
     @property
     def capacity(self):
