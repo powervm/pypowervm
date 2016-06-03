3bf8911423b3ad306e51998ede7b3d881a196d08
Eric Fried <efried@us.ibm.com>
2016-06-03 11:14:28 -0500

    WIP: SRIOV 2 of ?: Adapter Phys ports & max VFs
    (SRIOV 1: 7186282a388d3368140bc629a092002c981d3384)
    
    This second change set for SRIOV enablement provides the following in
    pypowervm.wrappers.card:
    - SRIOVEthPPort: Wrapper around SRIOVEthernetPhysicalPort.  The
      following fields are included in this change set:
      - label (Label) (settable)
      - loc_code (LocationCode)
      - port_id (PhysicalPortID)
      - sublabel (SubLabel) (settable)
      - link_status (LinkStatus)
      - cfg_max_ports (ConfiguredMaxEthernetLogicalPorts) (settable)
      - min_granularity (MinimumEthernetCapacityGranularity)
      - supp_max_ports (MaxSupportedEthernetLogicalPorts)
      - allocated_capacity (AllocatedCapacity) (settable)
    - SRIOVConvPPort: Wrapper around SRIOVConvergedNetworkAdapterPhysicalPort, an
      empty subclass of SRIOVEthPPort (FCoE is not supported).
    - SRIOVAdapter.phys_ports: A consolidated list of all the adapter's
      SRIOV*PPort elements.
    
    Change-Id: I6764b0bb83905eebdee57a5010891c19eb00697e

--- pypowervm/tests/wrappers/test_iocard.py
+++ pypowervm/tests/wrappers/test_iocard.py
@@ -28,25 +28,87 @@ class TestSRIOVAdapter(twrap.TestWrapper):
 
     def setUp(self):
         super(TestSRIOVAdapter, self).setUp()
-        self.io_adpt = self.dwrap.asio_config.sriov_adapters[0]
+        self.sriovs = self.dwrap.asio_config.sriov_adapters
+
+    def test_list(self):
+        self.assertEqual(3, len(self.sriovs))
+        for sriov in self.sriovs:
+            self.assertIsInstance(sriov, card.SRIOVAdapter)
 
     def test_attrs(self):
         desc = 'PCIe2 4-port (10Gb FCoE & 1GbE) SR&RJ45 Adapter'
 
-        self.assertEqual('553713680', self.io_adpt.id)
-        self.assertEqual(desc, self.io_adpt.description)
-        self.assertEqual('U78CB.001.WZS06RG-P1-C7',
-                         self.io_adpt.phys_loc_code)
-        self.assertIsInstance(self.io_adpt, card.SRIOVAdapter)
+        self.assertEqual('553713696', self.sriovs[0].id)
+        self.assertEqual(desc, self.sriovs[0].description)
+        self.assertEqual('U78C7.001.RCH0004-P1-C8',
+                         self.sriovs[0].phys_loc_code)
 
     def test_mode(self):
-        self.assertEqual('Dedicated', self.io_adpt.mode)
+        self.assertEqual('Sriov', self.sriovs[0].mode)
         # Test setter
-        self.io_adpt.mode = 'unknown'
-        self.assertEqual('unknown', self.io_adpt.mode)
+        self.sriovs[0].mode = 'unknown'
+        self.assertEqual('unknown', self.sriovs[0].mode)
 
     def test_state(self):
-        self.assertEqual('NotConfigured', self.io_adpt.state)
+        self.assertEqual('Running', self.sriovs[0].state)
+
+    def test_physical_ports(self):
+        adapter = self.sriovs[0]
+
+        phyports = adapter.phys_ports()
+        self.assertEqual(4, len(phyports))
+
+        self.assertEqual(None, phyports[0].label)
+        phyports[0].label = 'updatedlabel'
+        self.assertEqual('updatedlabel', phyports[0].label)
+
+        self.assertEqual('U78C7.001.RCH0004-P1-C8-T1', phyports[0].loc_code)
+
+        self.assertEqual(0, phyports[0].port_id)
+
+        self.assertEqual(None, phyports[0].sublabel)
+        phyports[0].sublabel = 'updatedsublabel'
+        self.assertEqual('updatedsublabel', phyports[0].sublabel)
+
+        self.assertEqual(True, phyports[0].link_status)
+
+        self.assertEqual(20, phyports[0].cfg_max_ports)
+        phyports[0].cfg_max_ports = 40
+        self.assertEqual(40, phyports[0].cfg_max_ports)
+
+        self.assertEqual(200, phyports[0].min_granularity)
+
+        self.assertEqual(20, phyports[0].supp_max_ports)
+
+        self.assertEqual('2.0%', phyports[0].allocated_capacity)
+        phyports[0].allocated_capacity = '4.0%'
+        self.assertEqual('4.0%', phyports[0].allocated_capacity)
+
+        self.assertEqual(None, phyports[2].label)
+        phyports[2].label = 'updatedlabel'
+        self.assertEqual('updatedlabel', phyports[0].label)
+
+        self.assertEqual('U78C7.001.RCH0004-P1-C8-T3', phyports[2].loc_code)
+
+        self.assertEqual(2, phyports[2].port_id)
+
+        self.assertEqual(None, phyports[2].sublabel)
+        phyports[2].sublabel = 'updatedsublabel'
+        self.assertEqual('updatedsublabel', phyports[2].sublabel)
+
+        self.assertEqual(True, phyports[2].link_status)
+
+        self.assertEqual(4, phyports[2].cfg_max_ports)
+        phyports[2].cfg_max_ports = 40
+        self.assertEqual(40, phyports[2].cfg_max_ports)
+
+        self.assertEqual(200, phyports[2].min_granularity)
+
+        self.assertEqual(4, phyports[2].supp_max_ports)
+
+        self.assertEqual('2.0%', phyports[2].allocated_capacity)
+        phyports[2].allocated_capacity = '4.0%'
+        self.assertEqual('4.0%', phyports[2].allocated_capacity)
 
 if __name__ == "__main__":
     unittest.main()
