795b99a196c31868ae96f1d567caa6d659ca41b0
Brent Tang <btang@us.ibm.com>
2016-07-06 09:47:08 -0700

    Add VeNCrypt (TLS/x509) Security to the VNC Repeater
    Currently the VNC Repeater is used to be able to open up the LinuxVNC
    sessions (started by mkvterm) on a port for remote access.  However,
    there is no authentication or encryption done at all on these
    connections, so anyone that has access through the firewall can
    connect to these sessions and also possibly have access to the
    unencrypted data sent across.
    
    This changeset will provide support for optionally specifying
    an x509 certificate/key that will be used for x509/TLS security
    authentication between the client and the VNC Repeater to allow
    for authentication and encrypted transmission.  The VNC Repeater
    will continue to communicate with the LinuxVNC session locally
    without authentication or encryption.
    
    If a x509 certificate/key is specified, the VNC Repeater will
    enforce that the client negotiates to use VeNCrypt as part of
    the security handshake and then will wrapper the socket to use
    TLS 1.2 for secure communication.
    
    Change-Id: I8c4eb1dbe365697ab3592c6528e1c7df41814701

--- pypowervm/tasks/vterm.py
+++ pypowervm/tasks/vterm.py
@@ -18,7 +18,10 @@
 
 import re
 import select
+import six
 import socket
+import ssl
+import struct
 import subprocess
 import threading
 
@@ -116,7 +119,8 @@ def open_localhost_vnc_vterm(adapter, lpar_uuid):
 
 
 def open_remotable_vnc_vterm(
-        adapter, lpar_uuid, local_ip, remote_ips=None, vnc_path=None):
+        adapter, lpar_uuid, local_ip, remote_ips=None, vnc_path=None,
+        use_x509_auth=False, ca_certs=None, server_cert=None, server_key=None):
     """Opens a VNC vTerm to a given LPAR.  Wraps in some validation.
 
     Must run on the management partition.
@@ -143,6 +147,17 @@ def open_remotable_vnc_vterm(
 
                      If no vnc_path is specified, then no path is expected
                      to be passed in by the VNC client.
+    :param use_x509_auth: (Optional, Default: False) If enabled, uses X509
+                          Authentication for the VNC sessions started for VMs.
+    :param ca_certs: (Optional, Default: None) Path to CA certificate to
+                     use for verifying VNC X509 Authentication.  Only used
+                     if use_x509_auth is set to True.
+    :param server_cert: (Optional, Default: None) Path to Server certificate
+                        to use for verifying VNC X509 Authentication.  Only
+                        used if use_x509_auth is set to True.
+    :param server_key: (Optional, Default: None) Path to Server private key
+                       to use for verifying VNC X509 Authentication.  Only
+                       used if use_x509_auth is set to True.
     :return: The VNC Port that the terminal is running on.
     """
     # This API can only run if local.
@@ -161,6 +176,10 @@ def open_remotable_vnc_vterm(
             repeater = _VNCRepeaterServer(
                 lpar_uuid, local_ip, vnc_port, remote_ips=remote_ips,
                 vnc_path=vnc_path)
+            # If we are doing x509 Authentication, then setup the certificates
+            if use_x509_auth:
+                repeater.set_x509_certificates(
+                    ca_certs, server_cert, server_key)
             _LOCAL_VNC_SERVERS[vnc_port] = repeater
             _LOCAL_VNC_UUID_TO_PORT[lpar_uuid] = vnc_port
 
@@ -243,8 +262,23 @@ class _VNCRepeaterServer(threading.Thread):
         self.port = port
         self.vnc_path = vnc_path
         self.remote_ips = remote_ips
+        self.x509_certs = None
 
         self.alive = True
+
+    def set_x509_certificates(self, ca_certs=None,
+                              server_cert=None, server_key=None):
+        """Set the x509 Certificates to use for TLS authentication.
+
+        :param ca_certs: (Optional, Default: None) Path to CA certificate to
+                         use for verifying VNC X509 Authentication.
+        :param server_cert: (Optional, Default: None) Path to Server cert
+                            to use for verifying VNC X509 Authentication.
+        :param server_key: (Optional, Default: None) Path to Server private key
+                           to use for verifying VNC X509 Authentication.
+        """
+        self.x509_certs = dict(
+            ca_certs=ca_certs, server_cert=server_cert, server_key=server_key)
 
     def stop(self):
         """Stops the repeater from running."""
@@ -364,15 +398,130 @@ class _VNCRepeaterServer(threading.Thread):
                 client_socket.close()
                 return
 
+        # Setup the forwarding socket to the local LinuxVNC session
+        self._setup_forwarding_socket(peers, client_socket)
+
+    def _setup_forwarding_socket(self, peers, client_socket):
+        """Setup the forwarding socket to the local LinuxVNC session.
+
+        :param peers: The peer dictionary.  Will map the new client to the new
+                      forwarding port.
+        :param client_socket:  The client-side socket to receive data from.
+        """
         # Get the forwarder.  This will be the socket we read FROM the
         # localhost.  When this receives data, it will be sent to the client
         # socket.
         fwd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         fwd.connect(('127.0.0.1', self.port))
 
+        # If we were told to enable VeNCrypt using X509 Authentication, do so
+        if self.x509_certs is not None:
+            ssl_socket = self._enable_x509_authentication(client_socket, fwd)
+            # If there was an error enabling SSL, then close the sockets
+            if ssl_socket is None:
+                client_socket.close()
+                fwd.close()
+                return
+            client_socket = ssl_socket
+
         # Set them up as peers in the dictionary.  They will now be considered
         # as input sources.
         peers[fwd], peers[client_socket] = client_socket, fwd
+
+    def _enable_x509_authentication(self, client_socket, server_socket):
+        """Enables and Handshakes VeNCrypt using X509 Authentication.
+
+        :param client_socket:  The client-side socket to receive data from.
+        :param server_socket:  The server-side socket to forward data to.
+        :return ssl_socket:  A client-side socket wrappered for SSL or None
+                             if there is an error.
+        """
+        try:
+            # First perform the RFB Version negotiation between client/server
+            self._version_negotiation(client_socket, server_socket)
+            # Next perform the Security Authentication Type Negotiation
+            if not self._auth_type_negotiation(client_socket):
+                return None
+            # Next perform the Security Authentication SubType Negotiation
+            if not self._auth_subtype_negotiation(client_socket):
+                return None
+            # Now that the VeNCrypt handshake is done, do the SSL wrapper
+            ca_certs = self.x509_certs.get('ca_certs')
+            server_key = self.x509_certs.get('server_key')
+            server_cert = self.x509_certs.get('server_cert')
+            return ssl.wrap_socket(
+                client_socket, server_side=True, ca_certs=ca_certs,
+                certfile=server_cert, keyfile=server_key,
+                ssl_version=ssl.PROTOCOL_TLSv1_2, cert_reqs=ssl.CERT_REQUIRED)
+        # If we got an error, log and handle to not take down the thread
+        except Exception as exc:
+            LOG.warning(_("Error negotiating SSL for VNC Repeater: %s") % exc)
+            LOG.exception(exc)
+            return None
+
+    def _version_negotiation(self, client_socket, server_socket):
+        """Performs the RFB Version negotiation between client/server.
+
+        :param client_socket:  The client-side socket to receive data from.
+        :param server_socket:  The server-side socket to forward data to.
+        """
+        # Do a pass-thru of the RFB Version negotiation up-front
+        # The length of the version is 12, such as 'RFB 003.007\n'
+        client_socket.sendall(server_socket.recv(12))
+        server_socket.sendall(client_socket.recv(12))
+        # Since we are doing our own additional authentication
+        # just tell the server we are doing No Authentication (1) to it
+        server_socket.recv(six.byte2int(server_socket.recv(1)))
+        server_socket.sendall(six.int2byte(1))
+
+    def _auth_type_negotiation(self, client_socket):
+        """Performs the VeNCrypt Authentication Type Negotiation.
+
+        :param client_socket:  The client-side socket to receive data from.
+        :return success:  Boolean whether the handshake was successful.
+        """
+        # Do the VeNCrypt handshake next before establishing SSL
+        # Say we only support VeNCrypt (19) authentication version 0.2
+        client_socket.sendall(six.int2byte(1))
+        client_socket.sendall(six.int2byte(19))
+        client_socket.sendall("\x00\x02")
+        authtype = client_socket.recv(1)
+        # Make sure the Client supports the VeNCrypt (19) authentication
+        if len(authtype) < 1 or six.byte2int(authtype) != 19:
+            # Send a 1 telling the client the type wasn't accepted
+            client_socket.sendall(six.int2byte(1))
+            return False
+        vers = client_socket.recv(2)
+        # Make sure the Client supports at least version 0.2 of it
+        if ((len(vers) < 2 or six.byte2int(vers) != 0
+             or six.byte2int(vers[1:]) < 2)):
+            # Send a 1 telling the client the type wasn't accepted
+            client_socket.sendall(six.int2byte(1))
+            return False
+        # Tell the Client we have accepted the authentication type
+        # In this particular case 0 means the type was accepted
+        client_socket.sendall(six.int2byte(0))
+        return True
+
+    def _auth_subtype_negotiation(self, client_socket):
+        """Performs the x509None Authentication Sub-Type Negotiation.
+
+        :param client_socket:  The client-side socket to receive data from.
+        :return success:  Boolean whether the handshake was successful.
+        """
+        # Tell the client the authentication sub-type is x509None (260)
+        client_socket.sendall(six.int2byte(1))
+        client_socket.sendall(struct.pack('!I', 260))
+        subtyp_raw = client_socket.recv(4)
+        # Make sure that the client also supports sub-type x509None (260)
+        if 260 not in struct.unpack('!I', subtyp_raw):
+            # Send a 0 telling the client the sub-type wasn't accepted
+            client_socket.sendall(six.int2byte(0))
+            return False
+        # Tell the Client we have accepted the authentication handshake
+        # In this particular case 1 means the sub-type was accepted
+        client_socket.sendall(six.int2byte(1))
+        return True
 
     def _check_http_connect(self, client_socket, vnc_path):
         """Determines if the vnc_path matches the HTTP connect string.
