f01e8bc81d62f6b88f124c34ffd0554d35460d51
Eric Fried <efried@us.ibm.com>
2016-07-26 13:39:32 -0500

    pypowervm.tasks.sriov.find_pport
    Utility method pypowervm.tasks.sriov.find_pport locates an SR-IOV
    physical port within the SR-IOV adapters of a managed system object
    given its physical location code, and returns the SRIOVAdapter and
    SRIOV*PPort objects associated with it, or (None, None) if no such port
    exists.
    
    Change-Id: I79ba0c60029f191b3c935c2406f52bf9a395c2bd

--- pypowervm/tasks/sriov.py
+++ pypowervm/tasks/sriov.py
@@ -391,3 +391,21 @@ def safe_update_pports(sys_w, force=False):
             for warning in warnings:
                 LOG.warning(warning)
         sys_w.update()
+
+
+def find_pport(physloc, sys_w):
+    """Find an SR-IOV physical port based on its location code.
+
+    :param physloc: Physical location code string (per SRIOV*PPort.loc_code) of
+                    the SR-IOV physical port to find.
+    :param sys_w: pypowervm.wrappers.managed_system.System wrapper of the host.
+    :return: The SRIOVAdapter owning the found physical port, or None if no
+             such port exists in sys_w.
+    :return: SRIOVEthPPort or SRIOVConvPPort wrapper with the specified
+             location code, or None if no such port exists in sys_w.
+    """
+    for sriov in sys_w.asio_config.sriov_adapters:
+        for pport in sriov.phys_ports:
+            if pport.loc_code == physloc:
+                return sriov, pport
+    return None, None
