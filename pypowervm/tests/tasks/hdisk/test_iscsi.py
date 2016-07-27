e99422bd7d5162a1d7b604f578912ee858ca9462
Taylor Jakobson <tjakobs@us.ibm.com>
2016-07-27 13:47:41 -0500

    Add iscsi discovery method
    Add method to hdisk implementation to discover/login/query iscsi
    target. Requires connection information about target of iscsi.
    
    Add method to hdisk implementation to discover iscsi
    initiator name.
    
    Change-Id: Ia3f40480773afe2f98ec2085562bd2fbb2e4f5b9

---
+++ pypowervm/tests/tasks/hdisk/test_iscsi.py
@@ -0,0 +1,58 @@
+# Copyright 2015 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+import testtools
+
+import pypowervm.entities as ent
+from pypowervm.tasks.hdisk import _iscsi as iscsi
+import pypowervm.tests.test_fixtures as fx
+from pypowervm.wrappers import job
+
+
+class TestIscsi(testtools.TestCase):
+
+    def setUp(self):
+        super(TestIscsi, self).setUp()
+        entry = ent.Entry({}, ent.Element('Dummy', None), None)
+        self.mock_job = job.Job(entry)
+        self.adpt = self.useFixture(fx.AdapterFx()).adpt
+
+    @mock.patch('pypowervm.wrappers.job.Job.wrap')
+    @mock.patch('pypowervm.wrappers.job.Job.run_job')
+    @mock.patch('pypowervm.wrappers.job.Job.create_job_parameter')
+    @mock.patch('pypowervm.wrappers.job.Job.get_job_results_as_dict')
+    def test_discover_iscsi(self, mock_job_res, mock_job_p, mock_run_job,
+                            mock_job_w):
+        mock_job_w.return_value = self.mock_job
+        mock_host_ip = '10.0.0.1'
+        mock_user = 'username'
+        mock_pass = 'password'
+        mock_iqn = 'fake_iqn'
+        mock_uuid = 'uuid'
+        args = ['VirtualIOServer', mock_uuid]
+        kwargs = {'suffix_type': 'do', 'suffix_parm': ('ISCSIDiscovery')}
+        mock_job_res.return_value = {'DEV_OUTPUT': '["fake_iqn devName"]'}
+        device_name = iscsi.discover_iscsi(
+            self.adpt, mock_host_ip, mock_user, mock_pass, mock_iqn, mock_uuid)
+
+        self.adpt.read.assert_called_once_with(*args, **kwargs)
+        mock_job_p.assert_any_call('hostIP', mock_host_ip)
+        mock_job_p.assert_any_call('user', mock_user)
+        mock_job_p.assert_any_call('password', mock_pass)
+        self.assertEqual('devName', device_name)
+        self.assertEqual(1, mock_run_job.call_count)
+        self.assertEqual(4, mock_job_p.call_count)
