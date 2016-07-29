a9294c90436c9034cea392cb1c0e9aec9683c67a
Eric Larese <erlarese@us.ibm.com>
2016-07-29 12:20:00 -0400

    Add lock to ensure_vlan
    Multiple concurrent migrations can cause errors with network bridge
    updates. This changes adds a lock to prevent failures in these
    scenarios.
    
    Change-Id: I17a12b27b4a66fbef5542064bf026896904697bd
    Closes-Bug: #1607854

--- pypowervm/tasks/network_bridger.py
+++ pypowervm/tasks/network_bridger.py
@@ -21,6 +21,8 @@ import copy
 
 import six
 
+from oslo_concurrency import lockutils as lock
+
 from pypowervm import const as c
 from pypowervm import exceptions as pvm_exc
 from pypowervm import util as pvm_util
@@ -30,6 +32,7 @@ from pypowervm.wrappers import network as pvm_net
 from pypowervm.wrappers import virtual_io_server as pvm_vios
 
 _MAX_VLANS_PER_VEA = 20
+_ENSURE_VLAN_LOCK = 'ensure_vlans_nb'
 
 
 def ensure_vlans_on_nb(adapter, host_uuid, nb_uuid, vlan_ids):
@@ -129,7 +132,7 @@ class NetworkBridger(object):
         self.host_uuid = host_uuid
         self._orphan_map = None
 
-    @pvm_retry.retry(tries=60, delay_func=pvm_retry.STEPPED_RANDOM_DELAY)
+    @lock.synchronized(_ENSURE_VLAN_LOCK)
     def ensure_vlans_on_nb(self, nb_uuid, vlan_ids):
         """Will make sure that the VLANs are assigned to the Network Bridge.
 
@@ -154,6 +157,10 @@ class NetworkBridger(object):
         :param nb_uuid: The Network Bridge UUID.
         :param vlan_ids: The list of VLANs to ensure are on the Network Bridge.
         """
+        self._ensure_vlans_on_nb_synch(nb_uuid, vlan_ids)
+
+    @pvm_retry.retry(tries=60, delay_func=pvm_retry.STEPPED_RANDOM_DELAY)
+    def _ensure_vlans_on_nb_synch(self, nb_uuid, vlan_ids):
         # Ensure the VLANs are ints, not strings.
         vlan_ids = [int(x) for x in vlan_ids]
 
@@ -181,9 +188,9 @@ class NetworkBridger(object):
             for peer_nb in peer_nbs:
                 if peer_nb.supports_vlan(vlan_id):
                     # Remove the VLAN.
-                    self.remove_vlan_from_nb(peer_nb.uuid, vlan_id,
-                                             fail_if_pvid=True,
-                                             existing_nbs=nb_wraps)
+                    self._remove_vlan_from_nb_synch(peer_nb.uuid, vlan_id,
+                                                    fail_if_pvid=True,
+                                                    existing_nbs=nb_wraps)
                     break
 
             # If it is an arbitrary VLAN ID on our network.  This should be
@@ -197,7 +204,7 @@ class NetworkBridger(object):
                 new_a_vid = self._find_new_arbitrary_vid(
                     all_nbs_on_vs, others=other_vlans)
                 self._reassign_arbitrary_vid(vlan_id, new_a_vid, req_nb)
-                return self.ensure_vlans_on_nb(nb_uuid, vlan_ids)
+                return self._ensure_vlans_on_nb_synch(nb_uuid, vlan_ids)
 
             # At this point, we've done all the easy checks.  Next up is to
             # detect if it is an orphan.
@@ -218,7 +225,7 @@ class NetworkBridger(object):
         # The Load Groups on the Network Bridge should be correct.
         req_nb.update()
 
-    @pvm_retry.retry(tries=60, delay_func=pvm_retry.STEPPED_RANDOM_DELAY)
+    @lock.synchronized(_ENSURE_VLAN_LOCK)
     def remove_vlan_from_nb(self, nb_uuid, vlan_id, fail_if_pvid=False,
                             existing_nbs=None):
         """Will remove the VLAN from a given Network Bridge.
@@ -231,6 +238,12 @@ class NetworkBridger(object):
                              bridge wrappers.  If not provided, will gather
                              from the system directly.
         """
+        self._remove_vlan_from_nb_synch(nb_uuid, vlan_id, fail_if_pvid,
+                                        existing_nbs)
+
+    @pvm_retry.retry(tries=60, delay_func=pvm_retry.STEPPED_RANDOM_DELAY)
+    def _remove_vlan_from_nb_synch(self, nb_uuid, vlan_id, fail_if_pvid=False,
+                                   existing_nbs=None):
         # Ensure we're working with an integer
         vlan_id = int(vlan_id)
 
