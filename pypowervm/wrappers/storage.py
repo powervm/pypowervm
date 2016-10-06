d175ff27932a311565c4db3cd474d7b00c80018b
Eric Fried <efried@us.ibm.com>
2016-10-06 15:59:11 -0500

    FileIO wrapper
    This change set introduces pypowervm.wrappers.storage.FileIO, which is
    really just a <VirtualDisk/> whose bld method only accepts a 'label'.
    This is to be used by attaching it to a VSCSIMapping which, when PUT to
    the REST server, will actually create the File I/O object on the back
    end.
    
    We also introduce VDiskType to mirror VirtualDiskType.Enum; and set the
    FileIO's VirtualDiskType field to 'File' as appropriate.
    
    Change-Id: Ic0d931259bc0f455e4c2807637e0957a99cb5865

--- pypowervm/wrappers/storage.py
+++ pypowervm/wrappers/storage.py
@@ -22,6 +22,7 @@ import binascii
 from oslo_log import log as logging
 import six
 
+import pypowervm.const as c
 import pypowervm.exceptions as ex
 from pypowervm.i18n import _
 import pypowervm.util as u
@@ -40,9 +41,16 @@ _DISK_MAX_LOGICAL_VOLS = 'MaxLogicalVolumes'
 _DISK_PART_SIZE = 'PartitionSize'
 _DISK_VG = 'VolumeGroup'
 _DISK_UDID = UDID
+_DISK_TYPE = 'VirtualDiskType'
 _VDISK_EL_ORDER = [_DISK_CAPACITY, _DISK_LABEL, DISK_NAME,
                    _DISK_MAX_LOGICAL_VOLS, _DISK_PART_SIZE, _DISK_VG,
-                   _DISK_UDID]
+                   _DISK_UDID, _DISK_TYPE]
+
+
+class VDiskType(object):
+    """From VirtualDiskType.Enum."""
+    FILE = 'File'
+    LV = 'LogicalVolume'
 
 # Physical Volume Constants
 PVS = 'PhysicalVolumes'
@@ -636,9 +644,74 @@ class PV(ewrap.ElementWrapper):
         return None
 
 
+@ewrap.Wrapper.base_pvm_type
+class _VDisk(ewrap.ElementWrapper):
+    """Methods common to VDisk and FileIO."""
+
+    @property
+    def name(self):
+        return self._get_val_str(DISK_NAME)
+
+    @name.setter
+    def name(self, name):
+        self.set_parm_value(DISK_NAME, name)
+
+    @property
+    def label(self):
+        return self._get_val_str(_DISK_LABEL)
+
+    def _label(self, new_label):
+        self.set_parm_value(_DISK_LABEL, new_label)
+
+    @property
+    def capacity(self):
+        """Returns the capacity in GB (float)."""
+        return self._get_val_float(_DISK_CAPACITY)
+
+    @capacity.setter
+    def capacity(self, capacity):
+        self.set_float_gb_value(_DISK_CAPACITY, capacity)
+
+    @property
+    def udid(self):
+        return self._get_val_str(_DISK_UDID)
+
+    @property
+    def vdtype(self):
+        return self._get_val_str(_DISK_TYPE)
+
+    def _vdtype(self, val):
+        self.set_parm_value(_DISK_TYPE, val, attrib=c.ATTR_KSV150)
+
+
 @ewrap.ElementWrapper.pvm_type(DISK_ROOT, has_metadata=True,
                                child_order=_VDISK_EL_ORDER)
-class VDisk(ewrap.ElementWrapper):
+class FileIO(_VDisk):
+    """A special case of VirtualDisk representing a File I/O object.
+
+    Do not PUT (.create) this wrapper directly.  Attach it to a VSCSIMapping
+    and PUT that instead.
+    """
+    target_dev_type = VDiskTargetDev
+
+    @classmethod
+    def bld(cls, adapter, path):
+        """Creates a FileIO wrapper for inclusion in a VSCSIMapping.
+
+        :param adapter: A pypowervm.adapter.Adapter for the REST API.
+        :param path: The file system path of the File I/O object.
+        :return: An Element that can be attached to a VSCSIMapping to create a
+                 File I/O on the server.
+        """
+        fio = super(FileIO, cls)._bld(adapter)
+        fio._label(path)
+        fio._vdtype(VDiskType.FILE)
+        return fio
+
+
+@ewrap.ElementWrapper.pvm_type(DISK_ROOT, has_metadata=True,
+                               child_order=_VDISK_EL_ORDER)
+class VDisk(_VDisk):
     """A virtual disk that can be attached to a VM."""
     target_dev_type = VDiskTargetDev
 
@@ -671,34 +744,6 @@ class VDisk(ewrap.ElementWrapper):
         vd = super(VDisk, cls)._bld(adapter)
         vd.name = name
         return vd
-
-    @property
-    def name(self):
-        return self._get_val_str(DISK_NAME)
-
-    @name.setter
-    def name(self, name):
-        self.set_parm_value(DISK_NAME, name)
-
-    @property
-    def label(self):
-        return self._get_val_str(_DISK_LABEL)
-
-    def _label(self, new_label):
-        self.set_parm_value(_DISK_LABEL, new_label)
-
-    @property
-    def capacity(self):
-        """Returns the capacity in GB (float)."""
-        return self._get_val_float(_DISK_CAPACITY)
-
-    @capacity.setter
-    def capacity(self, capacity):
-        self.set_float_gb_value(_DISK_CAPACITY, capacity)
-
-    @property
-    def udid(self):
-        return self._get_val_str(_DISK_UDID)
 
     @property
     def vg_uri(self):
