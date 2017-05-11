1d1d328e1a86a355a04a33c32547b1535dac1e14
chhagarw <chhagarw@in.ibm.com>
2017-05-11 13:21:37 -0400

    WIP:Handling the ISCSI discovery return code
    ISCSIDiscovery job should handle the iscsiadm standard error codes
    and process the discovery accordingly.
    
    Change-Id: Ib2e585dc054c52d15b34d25e32b054ddffa7f701

--- pypowervm/tests/tasks/hdisk/test_iscsi.py
+++ pypowervm/tests/tasks/hdisk/test_iscsi.py
@@ -43,13 +43,16 @@ class TestIscsi(testtools.TestCase):
         mock_pass = 'password'
         mock_iqn = 'fake_iqn'
         mock_uuid = 'uuid'
+        mock_lun = '2'
         mock_trans_type = 'trans_type'
+        iscsi_data = ISCSIInputData('10.0.0.1', 'username', 'password', 'fake_iqn', '2', 'trans_type')
         args = ['VirtualIOServer', mock_uuid]
         kwargs = {'suffix_type': 'do', 'suffix_parm': ('ISCSIDiscovery')}
-        mock_job_res.return_value = {'DEV_OUTPUT': '["fake_iqn devName udid"]'}
-        device_name, udid = iscsi.discover_iscsi(
-            self.adpt, mock_host_ip, mock_user, mock_pass, mock_iqn, mock_uuid,
-            transport_type=mock_trans_type)
+        mock_job_res.return_value = {'DEV_OUTPUT': '["fake_iqn devName udid"]',
+                                     'RETURN_CODE': '0'}
+        device_name, udid = iscsi.discover_iscsi( self.adpt, iscsi_data)
+        #     self.adpt, mock_host_ip, mock_user, mock_pass, mock_iqn, mock_uuid,
+        #    mock_lun, transport_type=mock_trans_type)
 
         self.adpt.read.assert_called_once_with(*args, **kwargs)
         self.assertEqual('devName', device_name)
@@ -80,7 +83,8 @@ class TestIscsi(testtools.TestCase):
         mock_uuid = 'uuid'
         args = ['VirtualIOServer', mock_uuid]
         kwargs = {'suffix_type': 'do', 'suffix_parm': ('ISCSIDiscovery')}
-        mock_job_res.return_value = {'InitiatorName': 'fake_iqn'}
+        mock_job_res.return_value = {'InitiatorName': 'fake_iqn',
+                                     'RETURN_CODE': '0'}
         initiator = iscsi.discover_iscsi_initiator(self.adpt, mock_uuid)
 
         self.adpt.read.assert_called_once_with(*args, **kwargs)
