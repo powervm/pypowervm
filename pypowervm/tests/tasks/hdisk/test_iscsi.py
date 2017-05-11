7a254cd8f3d72fd18422e6a268911497996f1256
chhagarw <chhagarw@in.ibm.com>
2017-05-11 13:21:37 -0400

    Handling the ISCSI discovery return code
    ISCSIDiscovery job should handle the iscsiadm standard error codes
    and process the discovery accordingly.
    
    Change-Id: Ib2e585dc054c52d15b34d25e32b054ddffa7f701

--- pypowervm/tests/tasks/hdisk/test_iscsi.py
+++ pypowervm/tests/tasks/hdisk/test_iscsi.py
@@ -18,6 +18,7 @@ import mock
 import testtools
 
 import pypowervm.entities as ent
+from pypowervm import exceptions as pexc
 from pypowervm.tasks.hdisk import _iscsi as iscsi
 import pypowervm.tests.test_fixtures as fx
 from pypowervm.wrappers import job
@@ -43,13 +44,15 @@ class TestIscsi(testtools.TestCase):
         mock_pass = 'password'
         mock_iqn = 'fake_iqn'
         mock_uuid = 'uuid'
+        mock_lun = '2'
         mock_trans_type = 'trans_type'
         args = ['VirtualIOServer', mock_uuid]
-        kwargs = {'suffix_type': 'do', 'suffix_parm': ('ISCSIDiscovery')}
-        mock_job_res.return_value = {'DEV_OUTPUT': '["fake_iqn devName udid"]'}
+        kwargs = {'suffix_type': 'do', 'suffix_parm': 'ISCSIDiscovery'}
+        mock_job_res.return_value = {'DEV_OUTPUT': '["fake_iqn devName udid"]',
+                                     'RETURN_CODE': '0'}
         device_name, udid = iscsi.discover_iscsi(
             self.adpt, mock_host_ip, mock_user, mock_pass, mock_iqn, mock_uuid,
-            transport_type=mock_trans_type)
+            transport_type=mock_trans_type, lunid=mock_lun)
 
         self.adpt.read.assert_called_once_with(*args, **kwargs)
         self.assertEqual('devName', device_name)
@@ -58,19 +61,17 @@ class TestIscsi(testtools.TestCase):
         mock_job_p.assert_has_calls([
             mock.call('hostIP', mock_host_ip), mock.call('user', mock_user),
             mock.call('password', mock_pass), mock.call('targetIQN', mock_iqn),
-            mock.call('transportType', mock_trans_type)], any_order=True)
-        self.assertEqual(5, mock_job_p.call_count)
+            mock.call('transportType', mock_trans_type),
+            mock.call('targetLUN', mock_lun)], any_order=True)
+        self.assertEqual(6, mock_job_p.call_count)
 
         mock_trans_type = None
-        mock_job_p.reset_mock()
-        iscsi.discover_iscsi(
-            self.adpt, mock_host_ip, mock_user, mock_pass, mock_iqn, mock_uuid,
-            transport_type=mock_trans_type)
-        mock_job_p.assert_has_calls([
-            mock.call('hostIP', mock_host_ip), mock.call('user', mock_user),
-            mock.call('password', mock_pass),
-            mock.call('targetIQN', mock_iqn)], any_order=True)
-        self.assertEqual(4, mock_job_p.call_count)
+
+        mock_job_res.return_value = {'DEV_OUTPUT': '["fake_iqn devName udid"]',
+                                     'RETURN_CODE': '8'}
+        self.assertRaises(pexc.ISCSIDiscoveryFailed, iscsi.discover_iscsi,
+                          self.adpt, mock_host_ip, mock_user, mock_pass,
+                          mock_iqn, mock_uuid, transport_type=mock_trans_type)
 
     @mock.patch('pypowervm.wrappers.job.Job.wrap')
     @mock.patch('pypowervm.wrappers.job.Job.run_job')
@@ -80,7 +81,8 @@ class TestIscsi(testtools.TestCase):
         mock_uuid = 'uuid'
         args = ['VirtualIOServer', mock_uuid]
         kwargs = {'suffix_type': 'do', 'suffix_parm': ('ISCSIDiscovery')}
-        mock_job_res.return_value = {'InitiatorName': 'fake_iqn'}
+        mock_job_res.return_value = {'InitiatorName': 'fake_iqn',
+                                     'RETURN_CODE': '0'}
         initiator = iscsi.discover_iscsi_initiator(self.adpt, mock_uuid)
 
         self.adpt.read.assert_called_once_with(*args, **kwargs)
@@ -100,6 +102,7 @@ class TestIscsi(testtools.TestCase):
         mock_job_p.return_value = mock_parm
         args = ['VirtualIOServer', mock_uuid]
         kwargs = {'suffix_type': 'do', 'suffix_parm': ('ISCSILogout')}
+        mock_job_res.return_value = {'RETURN_CODE': '0'}
         iscsi.remove_iscsi(self.adpt, mock_iqn, mock_uuid)
 
         self.adpt.read.assert_called_once_with(*args, **kwargs)
