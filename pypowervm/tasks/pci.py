6f3cd4f21b3f2e03f92ed08833b88a1c458d4b1e
Eric Fried <efried@us.ibm.com>
2017-08-18 14:15:22 -0500

    Utilities for IOSlot assignment information
    As part of the PCI passthrough effort, we need to know which I/O slots
    are assigned, and to which partitions.
    
    This change set introduces a new module,
    
        pypowervm.tasks.pci
    
    initially seeded with a class called
    
        SlotInfo
    
    This class, when instantiated, is a tuple of IOSlot wrappers with a
    couple of special attributes/properties in addition to their usual
    complement (drc_index, drc_name, etc.):
    
        assigned:  True if the slot is assigned to a partition; False
                   otherwise.
        part_uuid: The UUID of the partition to which the slot is assigned;
                   None if the slot is unassigned.
        part_w:    The (LPAR or VIOS) wrapper of the partition to which the
                   slot is assigned; None if the slot is unassigned.
    
    Partial-Implements: bp pci-passthrough
    
    Change-Id: Ia1cfced18b5fc70b000db08553d7b9534fcb8525

---
+++ pypowervm/tasks/pci.py
@@ -0,0 +1,140 @@
+# Copyright 2017 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Complex tasks around I/O slots and (non-SR-IOV) PCI devices."""
+
+import pypowervm.exceptions as exc
+import pypowervm.wrappers.base_partition as bp
+import pypowervm.wrappers.logical_partition as lpar
+import pypowervm.wrappers.managed_system as msys
+import pypowervm.wrappers.virtual_io_server as vios
+
+
+class _SlotAssignment(object):
+    """IOSlot "subclass" with info about the partition to which it is assigned.
+
+    We don't actually subclass IOSlot because there are two different IOSlot
+    wrappers: base_partition.IOSlot and managed_system.IOSlot.  They have most
+    of the same methods, so the consumer generally won't know or care which one
+    they're getting.
+    """
+    def __init__(self, ioslot_w, part=None):
+        """Create a _SlotAssignment.
+
+        :param ioslot_w: IOSlot (from System, LPAR, or VIOS) whose assignment
+                         information is being recorded.
+        :param part: LPAR or VIOS wrapper or UUID of the partition to which the
+                     ioslot_w is assigned; or None if the slot is unassigned.
+        """
+        self._ioslot_w = ioslot_w
+        self._adap = ioslot_w.adapter
+        _is_wrapper = isinstance(part, bp.BasePartition)
+        self._part_w = part if _is_wrapper else None
+        self.part_uuid = part.uuid if _is_wrapper else part
+
+    def __getattr__(self, item):
+        """Any properties not in init or defined below are on the IOSlot."""
+        return getattr(self._ioslot_w, item)
+
+    def _get_part(self, klass, uuid):
+        try:
+            return klass.get(self._adap, uuid=uuid)
+        except exc.HttpNotFound:
+            return None
+
+    @property
+    def part_w(self):
+        """The wrapper of the LPAR or VIOS to which this slot is assigned.
+
+        Will return None if the slot is unassigned.
+
+        :raise: NoPartitionForSlotAssignment if a partition UUID is associated
+                with this slot, but no partition exists for that UUID.  This
+                *should* mean that the SlotInfo is stale (the slot was
+                unassigned from the partition and the partition deleted since
+                the SlotInfo was retrieved).
+        """
+        if self._part_w is None:
+            if self.part_uuid is None:
+                return None
+            self._part_w = (self._get_part(lpar.LPAR, self.part_uuid) or
+                            self._get_part(vios.VIOS, self.part_uuid))
+            if self._part_w is None:
+                # This is an error.  It either means the SlotInfo needs to be
+                # refreshed; or there's really an assignment to a partition
+                # that doesn't exist.
+                raise exc.NoPartitionForSlotAssignment(
+                    uuid=self.part_uuid, drc_name=self._ioslot_w.drc_name)
+        return self._part_w
+
+    @property
+    def assigned(self):
+        return self.part_uuid is not None
+
+
+class SlotInfo(tuple):
+    """Information about IOSlots and the partitions to which they are assigned.
+
+    Usage:
+    # Set it up
+    slotinfo = SlotInfo(adapter, sys_w=my_sys_wrapper)
+    # Or to have SlotInfo retrieve the Managed System wrapper:
+    slotinfo = SlotInfo(adapter)
+
+    The result is a tuple - it cannot be modified (or "refreshed") once
+    created.  However, the order of the slots therein is not significant.
+
+    # How many slots?
+    numslots = len(slotinfo)
+    # Only unassigned slots
+    freeslots = [si for si in slotinfo if not si.assigned]
+    # First slot's partition UUID (will be None if unassigned)
+    part_uuid = slotinfo[0].part_uuid
+    # First slot's partition wrapper (will be None if unassigned)
+    part_w = slotinfo[0].part_w
+    # First slot's DRC index.
+    drc_index = slotinfo[0].drc_index
+    """
+    def __new__(cls, adap, sys_w=None):
+        """Create a new instance of SlotInfo.
+
+        :param adap: pypowervm.adapter.Adapter for REST API communication.
+        :param sys_w: pypowervm.wrappers.managed_system.System wrapper.  If not
+                      supplied, it will be retrieved afresh.
+        :return: A newly-initialized instance of SlotInfo.
+        """
+        if not sys_w:
+            sys_w = msys.System.get(adap)[0]
+        # List of _SlotAssignment.  Will be used to initialize this tuple.
+        sa_list = []
+        # TODO(efried) Redo this when sys_w's IOSlots contain partition info.
+        # Get all the partitions.
+        lfeed = lpar.LPAR.get(adap)
+        vfeed = vios.VIOS.get(adap)
+        # Set of DRC indices for filtering assigned slots out of sys_w later
+        seen_slots = set()
+        for part_w in lfeed + vfeed:
+            for slot in part_w.io_config.io_slots:
+                sa_list.append(_SlotAssignment(slot, part=part_w))
+                # These should never duplicate for assigned slots.
+                seen_slots.add(slot.drc_index)
+        # Now go through the Managed System and add only unassigned slots
+        for slot in sys_w.asio_config.io_slots:
+            if slot.drc_index not in seen_slots:
+                sa_list.append(_SlotAssignment(slot))
+
+        # Construct the tuple
+        return super(SlotInfo, cls).__new__(cls, sa_list)
---
+++ pypowervm/tests/tasks/test_pci.py
@@ -0,0 +1,172 @@
+# Copyright 2017 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import fixtures
+import testtools
+
+import pypowervm.exceptions as exc
+from pypowervm.tasks import pci
+import pypowervm.tests.tasks.util as tutil
+from pypowervm.tests import test_fixtures as pvm_fx
+import pypowervm.wrappers.logical_partition as lpar
+import pypowervm.wrappers.managed_system as msys
+import pypowervm.wrappers.virtual_io_server as vios
+
+
+SYS_FILE = 'managedsystem.txt'
+VIOS_FILE = 'fake_vios_feed.txt'
+LPAR_FILE = 'fake_lpar_feed.txt'
+
+TOTAL_SLOTS = 26
+V1_ASSIGNED_SLOTS = 3
+V2_ASSIGNED_SLOTS = 3
+LPAR_ASSIGNED_SLOTS = 0
+ASSIGNED_SLOTS = V1_ASSIGNED_SLOTS + V2_ASSIGNED_SLOTS + LPAR_ASSIGNED_SLOTS
+FREE_SLOTS = TOTAL_SLOTS - ASSIGNED_SLOTS
+KNOWN_DRC_INDEX = 553975841
+KNOWN_DRC_NAME = 'U5294.001.CEC1234-P13-C115'
+
+
+class TestPCI(testtools.TestCase):
+    def setUp(self):
+        super(TestPCI, self).setUp()
+
+        self.adap = self.useFixture(pvm_fx.AdapterFx()).adpt
+        self.sfeed = msys.System.wrap(tutil.load_file(
+            SYS_FILE, adapter=self.adap))
+        self.vfeed = vios.VIOS.wrap(tutil.load_file(
+            VIOS_FILE, adapter=self.adap))
+        self.lfeed = lpar.LPAR.wrap(tutil.load_file(
+            LPAR_FILE, adapter=self.adap))
+
+        self.mock_sys = self.useFixture(fixtures.MockPatch(
+            'pypowervm.tasks.pci.msys.System.get')).mock
+        self.mock_sys.return_value = self.sfeed
+
+        self.mock_vios = self.useFixture(fixtures.MockPatch(
+            'pypowervm.tasks.pci.vios.VIOS.get')).mock
+        self.mock_vios.return_value = self.vfeed
+
+        self.mock_lpar = self.useFixture(fixtures.MockPatch(
+            'pypowervm.tasks.pci.lpar.LPAR.get')).mock
+        self.mock_lpar.return_value = self.lfeed
+
+    def test_init_fetch(self):
+        pci.SlotInfo(self.adap)
+        self.mock_sys.assert_called_once_with(self.adap)
+        self.mock_vios.assert_called_once_with(self.adap)
+        self.mock_lpar.assert_called_once_with(self.adap)
+
+    def test_init_no_fetch(self):
+        slot_info = pci.SlotInfo(self.adap, sys_w=self.sfeed[0])
+        self.mock_sys.assert_not_called()
+        self.mock_vios.assert_called_once_with(self.adap)
+        self.mock_lpar.assert_called_once_with(self.adap)
+        # Sanity sniff test.
+        self.assertEqual(TOTAL_SLOTS, len(slot_info))
+
+    def test_tupleness(self):
+        slot_info = pci.SlotInfo(self.adap)
+        self.assertIsInstance(slot_info, tuple)
+        self.assertEqual(TOTAL_SLOTS, len(slot_info))
+        # Verify uniqueness
+        self.assertEqual(TOTAL_SLOTS,
+                         len({slot.drc_index for slot in slot_info}))
+        # Iteration works
+        for slot in slot_info:
+            self.assertIsInstance(slot, pci._SlotAssignment)
+        # Slicing works
+        slc = slot_info[4:15]
+        self.assertEqual(11, len(slc))
+        for slot in slc:
+            self.assertIsInstance(slot, pci._SlotAssignment)
+
+    def test_assigned(self):
+        assigned = [slot for slot in pci.SlotInfo(self.adap) if slot.assigned]
+        self.assertEqual(ASSIGNED_SLOTS, len(assigned))
+        for slot in assigned:
+            self.assertIsNotNone(slot.part_uuid)
+            self.assertIn(slot.part_w, self.vfeed + self.lfeed)
+            self.assertIn(slot.part_uuid,
+                          [part.uuid for part in self.vfeed + self.lfeed])
+        self.assertEqual(V1_ASSIGNED_SLOTS,
+                         len([slot for slot in assigned
+                              if slot.part_uuid == self.vfeed[0].uuid]))
+        self.assertEqual(V2_ASSIGNED_SLOTS,
+                         len([slot for slot in assigned
+                              if slot.part_uuid == self.vfeed[1].uuid]))
+        self.assertEqual(LPAR_ASSIGNED_SLOTS,
+                         len([slot for slot in assigned
+                              if slot.part_w in self.lfeed]))
+
+    def test_free(self):
+        free = [slot for slot in pci.SlotInfo(self.adap) if not slot.assigned]
+        self.assertEqual(FREE_SLOTS, len(free))
+        for slot in free:
+            self.assertIsNone(slot.part_uuid)
+            self.assertIsNone(slot.part_w)
+
+    def test_slot_attrs(self):
+        slot_info = pci.SlotInfo(self.adap)
+        indices = {slot.drc_index for slot in slot_info}
+        for index in indices:
+            self.assertIsInstance(index, int)
+        names = {slot.drc_name for slot in slot_info}
+        for name in names:
+            self.assertIsInstance(name, str)
+        self.assertIn(KNOWN_DRC_INDEX, indices)
+        self.assertIn(KNOWN_DRC_NAME, names)
+
+    def _set_up_slot_assignment(self):
+        sass = pci._SlotAssignment(self.sfeed[0].asio_config.io_slots[0], 'id')
+        # GETs not called yet
+        self.mock_lpar.assert_not_called()
+        self.mock_vios.assert_not_called()
+        return sass
+
+    def test_slot_assignemnt_lazy_load_exception(self):
+        sass = self._set_up_slot_assignment()
+        self.mock_lpar.return_value = None
+        self.mock_vios.return_value = None
+        self.assertRaises(exc.NoPartitionForSlotAssignment,
+                          lambda: sass.part_w)
+        self.mock_lpar.assert_called_once_with(self.adap, uuid='id')
+        self.mock_vios.assert_called_once_with(self.adap, uuid='id')
+
+    def test_slot_assignment_lazy_load_lpar(self):
+        sass = self._set_up_slot_assignment()
+        # Finds the LPAR - VIOS GET not called
+        self.mock_lpar.return_value = 'lpar wrapper'
+        self.assertEqual('lpar wrapper', sass.part_w)
+        self.mock_lpar.assert_called_once_with(self.adap, uuid='id')
+        self.mock_vios.assert_not_called()
+        # And it caches
+        self.assertEqual('lpar wrapper', sass.part_w)
+        self.mock_lpar.assert_called_once_with(self.adap, uuid='id')
+        self.mock_vios.assert_not_called()
+
+    def test_slot_assignment_lazy_load_vios(self):
+        sass = self._set_up_slot_assignment()
+        # Finds the VIOS - LPAR get is called first
+        self.mock_lpar.return_value = None
+        self.mock_vios.return_value = 'vios wrapper'
+        self.assertEqual('vios wrapper', sass.part_w)
+        self.mock_lpar.assert_called_once_with(self.adap, uuid='id')
+        self.mock_vios.assert_called_once_with(self.adap, uuid='id')
+        # And it caches
+        self.assertEqual('vios wrapper', sass.part_w)
+        self.mock_lpar.assert_called_once_with(self.adap, uuid='id')
+        self.mock_vios.assert_called_once_with(self.adap, uuid='id')
