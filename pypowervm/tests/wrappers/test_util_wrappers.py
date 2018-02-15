3c4bae82109fbdc77ea89c2f88cdc40bde1b92ed
Eric Fried <efried@us.ibm.com>
2018-02-15 17:51:35 -0600

    Declarative WrapperElemList finder utility
    Introduce pypowervm.utils.wrappers.WrapperElemListFilter, allowing
    filtering of a WrapperElemList by (nested) property values.
    
    For a WrapperElemList (such as VIOS.vfc_mappings), use this class to
    create and run a declarative filter to isolate some number of the
    ElementWrappers therein.
    
    Usage:
    - Initialize the filter with the WrapperElemList
    
        filt = WrapperElemListFilter(vios_wrapper.vfc_mappings)
    
    - Filter by a @property of each ElementWrapper by using a method with
      the same name as the @property.  The (single) parameter is the value
      to filter for, in whatever form it would be returned by that
      @property.
    
        filt.client_lpar_href(href_string)
    
    - Filter by a *nested* @property of each ElementWrapper.  Again, the
      name of the method should be the same as the @property returning the
      nested ElementWrapper.  The parameters must be keyword arguments where
      the key is the name of the @property of the sub-ElementWrapper, and
      the value is the value to filter for, in whatever form it would be
      returned by the (sub-)@property.
    
        filt.client_adapter(wwpns=['C05076079CFF0E56', 'C05076079CFF0E57'])
    
    - Results of the filtering are available at any point via the .results
      attribute (which had better never be the name of an ElementWrapper
      @property).  The results will always be a list, a subset of the
      original WrapperElemList given to the constructor.  It may be
      identical to the original; or empty; or anywhere in between.
    
        for wrapper in filt.results:
            # Do whatever
    
    - You cannot use both flat and nested syntax at the same time.  That
      wouldn't make sense anyway.
    
            # Raises ValueError:
            filt.client_adapter(href_string, wwpns=[])
    
    - You cannot search for more than one flat value at once.  That wouldn't
      make sense either.  (Unless maybe someday we want to make that signify
      a logical OR.)
    
            # Raises ValueError:
            filt.client_lpar_href(href_one, href_two)
    
    - All filters are cumulative and logically ANDed, including multiple
      kwargs to a single nested filter.
    - Filter methods return the filter, so they can be chained.
    
            results = WrapperElemListFilter(wrapper.things)
                      .flat_prop1(val)
                      .flat_prop2(val)
                      .nested_prop1(subprop=val)
                      .nested_prop2(subprop=val)
                      .results
    
    Change-Id: If134c100e0221f89e1220d25b5d75da7ef473db7

---
+++ pypowervm/tests/wrappers/test_util_wrappers.py
@@ -0,0 +1,79 @@
+# Copyright 2018 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import pypowervm.tests.test_utils.test_wrapper_abc as twrap
+from pypowervm.utils import wrappers
+import pypowervm.wrappers.virtual_io_server as vios
+
+
+class TestWrapperElemListFilter(twrap.TestWrapper):
+    file = 'fake_vios_feed.txt'
+    wrapper_class_to_test = vios.VIOS
+
+    href = ('https://9.1.2.3:12443/rest/api/uom/ManagedSystem/'
+            'e7344c5b-79b5-3e73-8f64-94821424bc25/LogicalPartition/'
+            '3ADDED46-B3A9-4E12-B6EC-8223421AF49B')
+    wwpns = ['C05076079CFF0E56', 'C05076079CFF0E57']
+
+    def test_filter(self):
+        filt = wrappers.WrapperElemListFilter(self.dwrap.vfc_mappings)
+        self.assertEqual(filt.results, self.dwrap.vfc_mappings)
+        filt.client_lpar_href(self.href)
+        expected = [wrap for wrap in self.dwrap.vfc_mappings if
+                    wrap.client_lpar_href == self.href]
+        self.assertEqual(expected, filt.results)
+        self.assertEqual(4, len(filt.results))
+        filt.client_adapter(wwpns=self.wwpns)
+        expected = [wrap for wrap in expected if
+                    wrap.client_adapter.wwpns == self.wwpns]
+        self.assertEqual(expected, filt.results)
+        self.assertEqual(1, len(filt.results))
+
+    def test_cumulative(self):
+        filt = wrappers.WrapperElemListFilter(self.dwrap.vfc_mappings)
+        self.assertEqual(58, len(filt.results))
+        filt.server_adapter(lpar_slot_num=4)
+        self.assertEqual(28, len(filt.results))
+        filt.client_lpar_href(self.href)
+        self.assertEqual(3, len(filt.results))
+        # Do the filters in reverse order to prove that they overlap
+        filt = wrappers.WrapperElemListFilter(self.dwrap.vfc_mappings)
+        self.assertEqual(58, len(filt.results))
+        filt.client_lpar_href(self.href)
+        self.assertEqual(4, len(filt.results))
+        filt.server_adapter(lpar_slot_num=4)
+        self.assertEqual(3, len(filt.results))
+
+    def test_chaining(self):
+        filt = wrappers.WrapperElemListFilter(self.dwrap.vfc_mappings)
+        self.assertEqual(
+            [wrap for wrap in self.dwrap.vfc_mappings if
+             wrap.client_lpar_href == self.href and
+             wrap.client_adapter.wwpns == self.wwpns],
+            filt.client_lpar_href(
+                self.href).client_adapter(wwpns=self.wwpns).results)
+        self.assertEqual(1, len(filt.results))
+
+    def test_errors(self):
+        filt = wrappers.WrapperElemListFilter(self.dwrap.vfc_mappings)
+        # Bogus flat prop
+        self.assertRaises(AttributeError, lambda: filt.bogus('foo'))
+        # Bogus sub-prop
+        self.assertRaises(AttributeError, filt.client_adapter, bogus=1)
+        # Can't specify both args and kwargs
+        self.assertRaises(ValueError, filt.client_adapter, 'foo', wwpns='bar')
+        # Can't specify multiple values
+        self.assertRaises(ValueError, filt.client_lpar_href, 'foo', 'bar')
