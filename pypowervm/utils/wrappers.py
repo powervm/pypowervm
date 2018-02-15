9dda660348f2af3512782d6341ce240cc38da7e3
Eric Fried <efried@us.ibm.com>
2018-02-15 17:51:35 -0600

    Declarative WrapperElemList finder utility
    Introduce pypowervm.utils.wrappers.WrapperElemListFilter, allowing
    filtering of a WrapperElemList by (nested) property values.
    
    For a WrapperElemList (such as VIOS.vfc_mappings), use this class to
    create and run a declarative filter to isolate some number of the
    ElementWrappers therein.
    
    Usage:
    - Initialize the filter with the WrapperElemList
    
        filt = WrapperElemListFilter(vios_wrapper.vfc_mappings)
    
    - Filter by a @property of each ElementWrapper by using a method with
      the same name as the @property.  The (single) parameter is the value
      to filter for, in whatever form it would be returned by that
      @property.
    
        filt.client_lpar_href(href_string)
    
    - Filter by a *nested* @property of each ElementWrapper.  Again, the
      name of the method should be the same as the @property returning the
      nested ElementWrapper.  The parameters must be keyword arguments where
      the key is the name of the @property of the sub-ElementWrapper, and
      the value is the value to filter for, in whatever form it would be
      returned by the (sub-)@property.
    
        filt.client_adapter(wwpns=['C05076079CFF0E56', 'C05076079CFF0E57'])
    
    - WrapperElemListFilter subclasses list, so all typical list
      operations are available.  The list is dynamically trimmed down to
      the results of whatever filtering is performed.
    
        for wrapper in filt:
            # Do whatever
    
    - You cannot use both flat and nested syntax at the same time.  That
      wouldn't make sense anyway.
    
            # Raises ValueError:
            filt.client_adapter(href_string, wwpns=[])
    
    - You cannot search for more than one flat value at once.  That wouldn't
      make sense either.  (Unless maybe someday we want to make that signify
      a logical OR.)
    
            # Raises ValueError:
            filt.client_lpar_href(href_one, href_two)
    
    - All filters are cumulative and logically ANDed, including multiple
      kwargs to a single nested filter.
    
    - Filter methods return the filter, so they can be chained.
    
            results = WrapperElemListFilter(wrapper.things)
                      .flat_prop1(val)
                      .flat_prop2(val)
                      .nested_prop1(subprop=val)
                      .nested_prop2(subprop=val)
    
    Change-Id: If134c100e0221f89e1220d25b5d75da7ef473db7

--- pypowervm/utils/wrappers.py
+++ pypowervm/utils/wrappers.py
@@ -20,6 +20,7 @@ import importlib
 import os
 import sys
 
+from pypowervm.i18n import _
 import pypowervm.wrappers.entry_wrapper as ewrap
 
 _wrappers_pkg = ('pypowervm', 'wrappers')
@@ -62,3 +63,94 @@ def wrapper_class_iter():
         except TypeError:
             # issubclass can't handle the things that aren't classes
             pass
+
+
+class WrapperElemListFilter(list):
+    """Filter a WrapperElemList by (nested) property values.
+
+    For a WrapperElemList (such as VIOS.vfc_mappings), use this class to create
+    and run a declarative filter to isolate some number of the ElementWrappers
+    therein.
+
+    Usage:
+    - Initialize the filter with the WrapperElemList
+
+        filt = WrapperElemListFilter(vios_wrapper.vfc_mappings)
+
+    - Filter by a @property of each ElementWrapper by using a method with the
+      same name as the @property.  The (single) parameter is the value to
+      filter for, in whatever form it would be returned by that @property.
+
+        filt.client_lpar_href(href_string)
+
+    - Filter by a *nested* @property of each ElementWrapper.  Again, the name
+      of the method should be the same as the @property returning the nested
+      ElementWrapper.  The parameters must be keyword arguments where the key
+      is the name of the @property of the sub-ElementWrapper, and the value is
+      the value to filter for, in whatever form it would be returned by the
+      (sub-)@property.
+
+        filt.client_adapter(wwpns=['C05076079CFF0E56', 'C05076079CFF0E57'])
+
+    - WrapperElemListFilter subclasses list, so all typical list operations
+      are available. The list is dynamically trimmed down to the results of
+      whatever filtering is performed.
+
+        for wrapper in filt:
+            # Do whatever
+
+    - You cannot use both flat and nested syntax at the same time.  That
+      wouldn't make sense anyway.
+
+            # Raises ValueError:
+            filt.client_adapter(href_string, wwpns=[])
+
+    - You cannot search for more than one flat value at once.  That wouldn't
+      make sense either.  (Unless maybe someday we want to make that signify a
+      logical OR.)
+
+            # Raises ValueError:
+            filt.client_lpar_href(href_one, href_two)
+
+    - All filters are cumulative and logically ANDed, including multiple kwargs
+      to a single nested filter.
+
+    - Filter methods return the filter, so they can be chained.
+
+            results = WrapperElemListFilter(wrapper.things)
+                      .flat_prop1(val)
+                      .flat_prop2(val)
+                      .nested_prop1(subprop=val)
+                      .nested_prop2(subprop=val)
+    """
+    @staticmethod
+    def _validate(afilt, kfilt):
+        # Don't allow both a value and subwrapper key=values
+        if afilt and kfilt:
+            raise ValueError(_("Specify a value or a subwrapper "
+                               "key=value, but not both."))
+        # Only one flat value allowed
+        if len(afilt) > 1:
+            raise ValueError(_("Only one value allowed."))
+
+    def __getattr__(self, item):
+        def filter_func(*afilt, **kfilt):
+            self._validate(afilt, kfilt)
+            # We build up the result list afresh
+            results = []
+            for wrapper in self:
+                # Let this raise AttributeError if item is bogus
+                val_or_subwrapper = getattr(wrapper, item)
+                if not val_or_subwrapper:
+                    continue
+                if kfilt and all(
+                        value == getattr(val_or_subwrapper, prop)
+                        for prop, value in kfilt.items()):
+                    results.append(wrapper)
+                elif afilt and afilt[0] == val_or_subwrapper:
+                    results.append(wrapper)
+            # filters are cumulative
+            self[0:len(self)] = results
+            # Allow chaining
+            return self
+        return filter_func
