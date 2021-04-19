ba2cc191f74462ece55af46bfbfcdc9de5fedd03
pkenchap <pkenchap@in.ibm.com>
2021-04-19 04:39:07 -0400

    SRIOV redundant pports list support
    User can deploy a VM with desire  redundant port, by sending the redundant
    pport list. If redundant pport list not sent , still existing
    functionality will work.
    
    Change-Id: I8bfcc689880b631a0c00a387a93da8cce88d75f0

--- pypowervm/tasks/sriov.py
+++ pypowervm/tasks/sriov.py
@@ -51,7 +51,7 @@ def _validate_capacity(min_capacity, max_capacity):
 
 def set_vnic_back_devs(vnic_w, pports, sys_w=None, vioses=None, redundancy=1,
                        capacity=None, max_capacity=None,
-                       check_port_status=False):
+                       check_port_status=False, redundent_pports=None):
     """Set a vNIC's backing devices over given SRIOV physical ports and VIOSes.
 
     Assign the backing devices to a iocard.VNIC wrapper using an anti-affinity
@@ -128,6 +128,12 @@ def set_vnic_back_devs(vnic_w, pports, sys_w=None, vioses=None, redundancy=1,
     :param max_capacity: (float) Maximum capacity to assign to each backing
                          device. Must be greater or equal to capacity and
                          less than 1.0.
+    :param redundent_pports: List of physical location code strings
+                             (corresponding to the loc_code @property of
+                             iocard.SRIOV*PPort) for all SRIOV redundant
+                             physical ports to be considered as backing
+                             devices for the vNIC. This does not mean that
+                             all of these ports will be used.
     :param check_port_status: If True, only ports with link-up status will be
                               considered for allocation.  If False (the
                               default), link-down ports may be used.
@@ -171,6 +177,17 @@ def set_vnic_back_devs(vnic_w, pports, sys_w=None, vioses=None, redundancy=1,
     # codes which have enough space for new VFs.
     pport_wraps = _get_good_pport_list(sriov_adaps, pports, capacity,
                                        redundancy, check_port_status)
+    if redundent_pports and (redundancy > 1):
+        card_use_backup = {}
+        redundant_pport_wraps = _get_good_pport_list(sriov_adaps,
+                                                     redundent_pports,
+                                                     capacity, redundancy,
+                                                     check_port_status)
+        for pport in redundant_pport_wraps:
+            said_b = pport.sriov_adap_id
+            if said_b not in card_use_backup:
+                card_use_backup[said_b] = {'num_used': 0, 'ports_left': 0}
+            card_use_backup[said_b]['ports_left'] += 1
 
     # At this point, we've validated enough that we won't raise.  Start by
     # clearing any existing backing devices.
@@ -204,6 +221,23 @@ def set_vnic_back_devs(vnic_w, pports, sys_w=None, vioses=None, redundancy=1,
         vnic_w.back_devs.append(card.VNICBackDev.bld(
             adap, vio.uuid, said, pp2use.port_id, capacity=capacity,
             max_capacity=max_capacity))
+        # If there any redundent_pports if the network tagged with redundant
+        # Physical network name, then we use these redundant pports as passive
+        # SRIOV adapter.
+        if redundent_pports and (redundancy > 1):
+            vio = vioses[vio_idx]
+            least_uses = min([cud['num_used'] for
+                              cud in card_use_backup.values()])
+            pp2use = min([pport for pport in redundant_pport_wraps if
+                          card_use_backup[
+                              pport.sriov_adap_id]['num_used'] == least_uses],
+                         key=lambda pp: pp.allocated_capacity)
+            said_b = pp2use.sriov_adap_id
+            # Create and add the backing device
+            vnic_w.back_devs.append(card.VNICBackDev.bld(
+                adap, vio.uuid, said_b, pp2use.port_id, capacity=capacity,
+                max_capacity=max_capacity))
+            break
         # Remove the port we just used from subsequent consideration.
         pport_wraps.remove(pp2use)
 
