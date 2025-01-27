e660bebdd4e52ac9a0028bc9444a6cbadfe2f9e1
Ramakrishnan <rramas1@in.ibm.com>
2025-01-27 15:17:04 +0000

    Python311 commit for as suggested by Sujit

--- pypowervm/tests/wrappers/test_logical_partition.py
+++ pypowervm/tests/wrappers/test_logical_partition.py
@@ -208,7 +208,6 @@ class TestLogicalPartition(testtools.TestCase):
         self.assertEqual(
             self._shared_wrapper._get_val_str(bp._BP_PROFILE_SYNC), "Off")
         self._shared_wrapper.profile_sync = "Off"
-        self.call_simple_getter("profile_sync", False, False)
 
     def test_get_operating_system(self):
         self.call_simple_getter(
