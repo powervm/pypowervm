4e67604ae056a51717daf8900218c7ac096a9427
sujithkumar <sujith7@ibm.com>
2025-04-29 15:24:05 +0530

    Added condition to check the port is listening for develop

--- pypowervm/tests/tasks/test_vterm.py
+++ pypowervm/tests/tasks/test_vterm.py
@@ -259,7 +259,7 @@ class TestVNCSocketListener(testtools.TestCase):
 
         self.srv_no_verify._new_client(mock_srv)
 
-        mock_s_sock.connect.assert_called_once_with(('127.0.0.1', '5800'))
+        mock_s_sock.connect_ex.assert_called_once_with(('127.0.0.1', '5800'))
 
         self.assertEqual({mock_c_sock: mock_s_sock, mock_s_sock: mock_c_sock},
                          self.rptr.peers)
@@ -278,7 +278,7 @@ class TestVNCSocketListener(testtools.TestCase):
 
         mock_c_sock.sendall.assert_called_once_with(
             b"HTTP/1.8 200 OK\r\n\r\n")
-        mock_s_sock.connect.assert_called_once_with(('127.0.0.1', '5800'))
+        mock_s_sock.connect_ex.assert_called_once_with(('127.0.0.1', '5800'))
 
         self.assertEqual({mock_c_sock: mock_s_sock, mock_s_sock: mock_c_sock},
                          self.rptr.peers)
@@ -296,7 +296,7 @@ class TestVNCSocketListener(testtools.TestCase):
 
         mock_c_sock.sendall.assert_called_once_with(
             b"HTTP/1.8 200 OK\r\n\r\n")
-        mock_s_sock.connect.assert_called_once_with(('127.0.0.1', '5800'))
+        mock_s_sock.connect_ex.assert_called_once_with(('127.0.0.1', '5800'))
 
         self.assertEqual({mock_c_sock: mock_s_sock, mock_s_sock: mock_c_sock},
                          self.rptr.peers)
