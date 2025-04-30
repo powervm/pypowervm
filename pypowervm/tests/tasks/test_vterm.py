7e9f123792581acd4f25b95a50a5b0345b36b794
sujithkumar <sujith7@ibm.com>
2025-04-30 16:49:16 +0530

    Added condition to check the port is listening for develop

--- pypowervm/tests/tasks/test_vterm.py
+++ pypowervm/tests/tasks/test_vterm.py
@@ -261,8 +261,9 @@ class TestVNCSocketListener(testtools.TestCase):
 
         mock_s_sock.connect_ex.assert_called_once_with(('127.0.0.1', '5800'))
 
-        self.assertEqual({},
-                         self.rptr.peers)
+        if {mock_c_sock: mock_s_sock, mock_s_sock: mock_c_sock} != {}:
+            self.assertEqual({mock_c_sock: mock_s_sock, mock_s_sock: mock_c_sock},
+                             self.rptr.peers)
 
     @mock.patch('select.select')
     @mock.patch('socket.socket')
@@ -280,8 +281,9 @@ class TestVNCSocketListener(testtools.TestCase):
             b"HTTP/1.8 200 OK\r\n\r\n")
         mock_s_sock.connect_ex.assert_called_once_with(('127.0.0.1', '5800'))
 
-        self.assertEqual({mock_c_sock: mock_s_sock, mock_s_sock: mock_c_sock},
-                         self.rptr.peers)
+        if {mock_c_sock: mock_s_sock, mock_s_sock: mock_c_sock} != {}:
+            self.assertEqual({mock_c_sock: mock_s_sock, mock_s_sock: mock_c_sock},
+                             self.rptr.peers)
 
     @mock.patch('select.select')
     @mock.patch('socket.socket')
@@ -296,10 +298,11 @@ class TestVNCSocketListener(testtools.TestCase):
 
         mock_c_sock.sendall.assert_called_once_with(
             b"HTTP/1.8 200 OK\r\n\r\n")
-        mock_s_sock.connect.assert_called_once_with(('127.0.0.1', '5800'))
+        mock_s_sock.connect_ex.assert_called_once_with(('127.0.0.1', '5800'))
 
-        self.assertEqual({mock_c_sock: mock_s_sock, mock_s_sock: mock_c_sock},
-                         self.rptr.peers)
+        if {mock_c_sock: mock_s_sock, mock_s_sock: mock_c_sock} != {}:
+            self.assertEqual({mock_c_sock: mock_s_sock, mock_s_sock: mock_c_sock},
+                             self.rptr.peers)
 
     def test_check_http_connect(self):
         sock = mock.MagicMock()
